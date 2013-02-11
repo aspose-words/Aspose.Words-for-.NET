7837892b3f50d3bcb32098c65ed1026d73ed84bc
Adam Skelton <adam.skelton@aspose.com>
2013-02-12 02:20:08 +1300

    added an example for how to get the page number of nodes in the document

---
+++ PageNumbersOfNodes/VisualBasic/My Project/AssemblyInfo.vb
@@ -0,0 +1,37 @@
+Imports Microsoft.VisualBasic
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+<Assembly: AssemblyTitle("PageNumberOfNodes")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyConfiguration("")>
+<Assembly: AssemblyCompany("Aspose")>
+<Assembly: AssemblyProduct("PageNumberOfNodes")>
+<Assembly: AssemblyCopyright("Copyright © Aspose 2013")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
+<Assembly: ComVisible(False)>
+
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("ddcfbee8-6ced-4e01-89a5-f5cfdd5d7be9")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' [assembly: AssemblyVersion("1.0.*")]
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
