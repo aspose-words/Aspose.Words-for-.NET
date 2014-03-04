f60982e0cc6142284f3c24198a74da0a5f633d3f
Adam Skelton <adam.skelton@aspose.com>
2014-03-05 04:25:21 +1300

    added test runner framework to base folder.

---
+++ ViewersAndVisualizers/ViewersAndVisualizersExamples.cs
@@ -0,0 +1,21 @@
+using Helpers;
+using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assert = NUnit.Framework.Assert;
+using Description = Microsoft.VisualStudio.TestTools.UnitTesting.DescriptionAttribute;
+
+namespace Examples.ViewersAndVisualizers
+{
+    [TestClass, TestFixture]
+    public class ViewersAndVisualizersExamples
+    {	
+        [TestMethod, Test, Owner("WinForm")]
+        public void DocumentExplorer()
+        {
+            TestHelper.SetDataDir("ViewersAndVisualizers/DocumentExplorer");
+
+            DocumentExplorerExample.Program.Main();
+        }
+
+    }
+}
