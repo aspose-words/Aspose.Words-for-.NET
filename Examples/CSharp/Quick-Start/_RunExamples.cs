f53081b1e236b993c43eb933dca69ad19dd929b9
Saqib Razzaq <saqib.razzaq@aspose.com>
2015-06-10 14:25:16 +0500

    Added quickstart in csharp examples

---
+++ Examples/CSharp/Quick-Start/_RunExamples.cs
@@ -0,0 +1,27 @@
+﻿using System.Reflection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace CSharp.Quick_Start
+{
+    class _RunExamples
+    {
+        public static void Main()
+        {
+            // Run the examples. Un-comment the one you want to run
+            AppendDocuments.Run();
+
+            // Stop before exiting
+            Console.WriteLine("\n\nProgram Finished. Press any key to exit....");
+            Console.ReadKey();
+        }
+
+        public static String GetDataDir()
+        {
+            return Path.GetFullPath("../../Quick-Start/Data/");
+        }
+    }
+}
