3e3518d7c5499cb6eaff20406b34d03a8b5be2b8
Zaheer Tariq <zaheer.tariq@aspose.com>
2015-07-03 14:27:22 +0500

    Initial commit - Aspose for NPOI

---
+++ Plugins/NPOI/Aspose.Words vs NPOI HWPF and XWPF/Aspose.Words/01-CreateEmptyDocument/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Aspose.Words;
+using System.IO;
+
+namespace CreateEmptyDocument
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //----------------------------------------------------
+            //  NPOI
+            //----------------------------------------------------            
+            //XWPFDocument doc = new XWPFDocument();
+            //doc.CreateParagraph();
+            //using (FileStream sw = File.Create("blank.docx"))
+            //{
+            //    doc.Write(sw);
+            //}
+
+
+            //----------------------------------------------------
+            //  Aspose.Words
+            //----------------------------------------------------
+            Document doc = new Document();
+            doc.Save("blank.docx");
+        }
+    }
+}
--- Plugins/README.md
+++ Plugins/README.md
@@ -3,4 +3,5 @@
 This section contains the following Platforms
 * [Aspose.Words Vs VSTO Word](Aspose.Words Vs VSTO Word)
 * [Microsoft Dynamics CRM](Dynamics CRM)
+* [NPOI - .NET version of POI Java project](NPOI)
 * [Visual Studio](Visual Studio)
