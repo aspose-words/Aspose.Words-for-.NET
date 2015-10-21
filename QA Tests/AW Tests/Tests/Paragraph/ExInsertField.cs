ded55180dc0f5ea5c1a9a42a9bcdd9453d6e94e9
vderyushev <falleretic@gmail.com>
2015-10-22 05:39:09 +1300

    Add tests and examples for API 15.08.0

---
+++ QA Tests/AW Tests/Tests/Paragraph/ExInsertField.cs
@@ -0,0 +1,81 @@
+﻿using Aspose.Words;
+using Aspose.Words.Fields;
+using NUnit.Framework;
+
+namespace QA_Tests.Tests
+{
+    [TestFixture]
+    internal class ExInsertField : QaTestsBase
+    {
+        [Test]
+        public void InsertField_FieldCode()
+        {
+            //ExStart
+            //ExFor:Paragraph
+            //ExFor:Paragraph.InsertField
+            //ExFor:Field
+            //ExSummary:Shows how to insert field using field code into the first paragraph
+            Document doc = new Document();
+
+            //Insert field using field code into the first paragraph
+            Paragraph para = doc.FirstSection.Body.FirstParagraph;
+            para.InsertField(" AUTHOR ", null, false);
+            //ExEnd
+        }
+
+        [Test]
+        public void InsertField_FieldType()
+        {
+            //ExStart
+            //ExFor:Paragraph
+            //ExFor:Paragraph.InsertField
+            //ExFor:Field
+            //ExSummary:Shows how to insert field using field code into the first paragraph
+            Document doc = new Document();
+
+            //Insert field using field type into the first paragraph
+            Paragraph para = doc.FirstSection.Body.FirstParagraph;
+            para.InsertField(FieldType.FieldAuthor, false, null, false);
+            //ExEnd
+        }
+
+        [Test]
+        public void InsertField_FieldCodeAndFieldValue()
+        {
+            //ExStart
+            //ExFor:Paragraph
+            //ExFor:Paragraph.InsertField
+            //ExFor:Field
+            //ExSummary:Shows how to insert field using field code and field value into the first paragraph
+            Document doc = new Document();
+
+            //Insert field using field code and field value into the first paragraph
+            Paragraph para = doc.FirstSection.Body.FirstParagraph;
+            para.InsertField(" AUTHOR ", "Test Field Value", null, false);
+            //ExEnd
+        }
+
+        [Test]
+        public void InsertField_RunText()
+        {
+            //ExStart
+            //ExFor:Paragraph
+            //ExFor:Paragraph.InsertField
+            //ExFor:Field
+            //ExSummary:Shows how to insert field befor/after a run of text
+            Document doc = new Document();
+
+            //Get the first paragraph of the document
+            Paragraph para = doc.FirstSection.Body.FirstParagraph;
+
+            //Add a run of text
+            Run run = new Run(doc) { Text = " Hello World!" };
+            para.AppendChild(run);
+
+            //Insert field befor a run of text
+            //For inserting field after a run of text, you must set "true" in the "isAfter" parameter
+            para.InsertField(" AUTHOR ", "Test Field Value", run, false);
+            //ExEnd
+        }
+    }
+}
---
+++ QA Tests/AW Tests/Tests/SaveOptions/Pdf/OutlineOptions.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using Aspose.Words;
+using Aspose.Words.Saving;
+using NUnit.Framework;
+
+namespace QA_Tests.Tests.SaveOptions.Pdf
+{
+    /// <summary>
+    /// Tests that verify saving to pdf using "CreateMissingOutlineLevels" parameter in "PdfSaveOptions"
+    /// </summary>
+    [TestFixture]
+    internal class OutlineOptions : QaTestsBase
+    {
+        [Test]
+        public void CreateMissingOutlineLevels()
+        {
+            Document doc = DocumentHelper.CreateDocumentFillWithDummyText();
+
+            PdfSaveOptions pdfSaveOptions = new PdfSaveOptions();
+
+            //Set maximum value of levels of headings
+            pdfSaveOptions.OutlineOptions.HeadingsOutlineLevels = 9;
+            pdfSaveOptions.OutlineOptions.CreateMissingOutlineLevels = true;
+            pdfSaveOptions.SaveFormat = SaveFormat.Pdf;
+
+            MemoryStream dstStream = new MemoryStream();
+            doc.Save(dstStream, pdfSaveOptions);
+
+            dstStream.Dispose();
+        }
+    }
+}
