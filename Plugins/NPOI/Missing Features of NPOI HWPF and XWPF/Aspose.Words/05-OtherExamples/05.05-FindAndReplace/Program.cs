646a998771416656f8d0b61eadb6b948d7f693b6
Assad Mahmood <assadvirgo@gmail.com>
2015-12-04 00:03:41 +0500

    Aspose.Words Features Missing in NPOI

---
+++ Plugins/NPOI/Missing Features of NPOI HWPF and XWPF/Aspose.Words/05-OtherExamples/05.05-FindAndReplace/Program.cs
@@ -0,0 +1,26 @@
+﻿using Aspose.Words;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05._05_FindAndReplace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Document doc = new Document();
+            DocumentBuilder builder = new DocumentBuilder(doc);
+
+            builder.Write("Hello World");
+
+            doc.Range.Replace("Hello", "Hallow", false, true);
+
+            String text = doc.Range.Text;
+
+            System.Console.WriteLine(text);
+            System.Console.ReadKey();
+        }
+    }
+}
