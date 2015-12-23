5bb3bbc3bba49600c0c0fea9e7561c5805423d65
romans91 <to.ru@Live.com>
2015-12-23 21:21:08 +1300

    New Examples
    SignEx

--- QA Tests/AW Tests/Examples/DigitalSignature/ExDigitalSignatureUtil.cs
+++ QA Tests/AW Tests/Examples/DigitalSignature/ExDigitalSignatureUtil.cs
@@ -5,6 +5,7 @@
 // "as is", without warranty of any kind, either expressed or implied.
 //////////////////////////////////////////////////////////////////////////
 
+using System;
 using NUnit.Framework;
 using QA_Tests.Tests;
 
@@ -24,6 +25,26 @@ namespace QA_Tests.Examples.Document
 
             string outputDocFileName = MyDir + "Document.NoSignatures.doc";
             Aspose.Words.DigitalSignatureUtil.RemoveAllSignatures(doc.OriginalFileName, outputDocFileName);            
+            //ExEnd
+        }
+
+        [Test]
+        // We don't include a sample certificate with the examples
+        // so this exception is expected instead since the file is not there.
+        [ExpectedException(typeof(System.IO.FileNotFoundException))]
+        public void SignEx()
+        {
+            //ExStart
+            //ExFor:RemoveAllSignatures
+            //ExId:RemoveAllSignaturesEx
+            //ExSummary:Shows how to use RemoveAllSignatures.
+            Aspose.Words.Document doc = new Aspose.Words.Document(MyDir + "Document.doc");
+
+            string outputDocFileName = MyDir + "Document.Signed.doc";
+
+            Aspose.Words.CertificateHolder ch = Aspose.Words.CertificateHolder.Create(MyDir + "MyPkcs12.pfx", "My password");
+
+            Aspose.Words.DigitalSignatureUtil.Sign(doc.OriginalFileName, outputDocFileName, ch, "My comment", DateTime.Now);
             //ExEnd
         }
     }
