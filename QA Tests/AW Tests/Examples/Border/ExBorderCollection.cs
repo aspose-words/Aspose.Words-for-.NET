f7927c1f3658edf1dd8de8db4f411d16c4a29c60
romans91 <to.ru@Live.com>
2015-12-31 10:47:14 +1300

    New Examples
    ExBorderCollection
    GetEnumeratorEx
    ClearFormattingEx

---
+++ QA Tests/AW Tests/Examples/Border/ExBorderCollection.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) 2001-2016 Aspose Pty Ltd. All Rights Reserved.
+//
+// This file is part of Aspose.Words. The source code in this file
+// is only intended as a supplement to the documentation, and is provided
+// "as is", without warranty of any kind, either expressed or implied.
+//////////////////////////////////////////////////////////////////////////
+
+using System;
+using NUnit.Framework;
+using QA_Tests.Tests;
+
+namespace QA_Tests.Examples.Border
+{
+    [TestFixture]
+    public class ExBorderCollection : QaTestsBase
+    {
+        [Test]
+        public void GetEnumeratorEx()
+        {
+            //ExStart
+            //ExFor:GetEnumerator
+            //ExId:GetEnumeratorEx
+            //ExSummary:Shows how to use GetEnumerator.
+            Aspose.Words.DocumentBuilder builder = new Aspose.Words.DocumentBuilder();
+            Aspose.Words.BorderCollection borders = builder.ParagraphFormat.Borders;
+
+            var enumerator = borders.GetEnumerator();
+            //ExEnd
+        }
+
+        [Test]
+        public void ClearFormattingEx()
+        {
+            //ExStart
+            //ExFor:ClearFormatting
+            //ExId:ClearFormattingEx
+            //ExSummary:Shows how to use ClearFormatting.
+            Aspose.Words.DocumentBuilder builder = new Aspose.Words.DocumentBuilder();
+            Aspose.Words.BorderCollection borders = builder.ParagraphFormat.Borders;
+
+            borders.ClearFormatting();
+            //ExEnd
+        }
+    }
+}
