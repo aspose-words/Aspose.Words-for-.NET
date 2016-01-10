c4ae3eea21c794e1867ca124bafd2decf6b34d7f
romans91 <to.ru@Live.com>
2016-01-10 21:12:28 +1300

    New Examples
    ExWarningInfoCollection
    GetEnumeratorEx

---
+++ QA Tests/AW Tests/Examples/Warning/ExWarningInfoCollection.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2001-2016 Aspose Pty Ltd. All Rights Reserved.
+//
+// This file is part of Aspose.Words. The source code in this file
+// is only intended as a supplement to the documentation, and is provided
+// "as is", without warranty of any kind, either expressed or implied.
+//////////////////////////////////////////////////////////////////////////
+
+using System;
+using NUnit.Framework;
+using QA_Tests.Tests;
+
+namespace QA_Tests.Examples.Warning
+{
+    [TestFixture]
+    public class ExWarningInfoCollection : QaTestsBase
+    {
+        [Test]
+        public void GetEnumeratorEx()
+        {
+            //ExStart
+            //ExFor:GetEnumerator
+            //ExId:GetEnumeratorEx
+            //ExSummary:Shows how to use GetEnumerator.
+            Aspose.Words.WarningInfoCollection wic = new Aspose.Words.WarningInfoCollection();
+            var enumerator = wic.GetEnumerator();
+            //ExEnd
+        }
+    }
+}
