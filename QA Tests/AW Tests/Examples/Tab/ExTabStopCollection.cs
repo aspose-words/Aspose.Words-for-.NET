3390283544d0a821d8d4773c3c23634608e88b96
romans91 <to.ru@Live.com>
2016-01-29 23:14:11 +1300

    Changed some TabStop examples to use ConvertUtil and added one more

--- QA Tests/AW Tests/Examples/Tab/ExTabStopCollection.cs
+++ QA Tests/AW Tests/Examples/Tab/ExTabStopCollection.cs
@@ -15,13 +15,14 @@ namespace QA_Tests.Examples.Tab
     public class ExTabStopCollection : QaTestsBase
     {
         [Test]
-        public void ClearAllAttrsEx()
+        public void ClearEx()
         {
             //ExStart
             //ExFor:TabStopCollection.Clear
-            //ExSummary:Shows how to clear a document of all tab stops.
+            //ExSummary:Shows how to remove all tab stops from a document.
             Aspose.Words.Document doc = new Aspose.Words.Document(ExDir + "Document.TableOfContents.doc");
 
+            // Clear all tab stops from every paragraph.
             foreach (Aspose.Words.Paragraph para in doc.GetChildNodes(Aspose.Words.NodeType.Paragraph, true))
             {
                 para.ParagraphFormat.TabStops.Clear();
@@ -37,16 +38,22 @@ namespace QA_Tests.Examples.Tab
             //ExStart
             //ExFor:TabStopCollection.Add(TabStop)
             //ExFor:TabStopCollection.Add(Double, TabAlignment, TabLeader)
-            //ExSummary:Shows how to create and add tabStop objects to a document.
+            //ExSummary:Shows how to create tab stops and add them to a document.
             Aspose.Words.Document doc = new Aspose.Words.Document(ExDir + "Document.doc");
             Aspose.Words.Paragraph paragraph = (Aspose.Words.Paragraph)doc.GetChild(Aspose.Words.NodeType.Paragraph, 0, true);
 
-            // Create a TabStop object and add it into the document
-            Aspose.Words.TabStop tabStop = new Aspose.Words.TabStop(84.99, Aspose.Words.TabAlignment.Left, Aspose.Words.TabLeader.Dashes);
+            // Create a TabStop object and add it to the document.
+            Aspose.Words.TabStop tabStop = new Aspose.Words.TabStop(Aspose.Words.ConvertUtil.InchToPoint(3), Aspose.Words.TabAlignment.Left, Aspose.Words.TabLeader.Dashes);
             paragraph.ParagraphFormat.TabStops.Add(tabStop);
 
-            // Add a TabStop without explicitly initializing  
-            paragraph.ParagraphFormat.TabStops.Add(169.98, Aspose.Words.TabAlignment.Left, Aspose.Words.TabLeader.Dashes);
+            // Add a tab stop without explicitly creating new TabStop objects.
+            paragraph.ParagraphFormat.TabStops.Add(Aspose.Words.ConvertUtil.MillimeterToPoint(100), Aspose.Words.TabAlignment.Left, Aspose.Words.TabLeader.Dashes);
+
+            // Add tab stops at 5 cm to all paragraphs.
+            foreach (Aspose.Words.Paragraph para in doc.GetChildNodes(Aspose.Words.NodeType.Paragraph, true))
+            {
+                para.ParagraphFormat.TabStops.Add(Aspose.Words.ConvertUtil.MillimeterToPoint(50), Aspose.Words.TabAlignment.Left, Aspose.Words.TabLeader.Dashes);
+            }
 
             doc.Save(ExDir + "Document.AddedTabStops Out.doc");
             //ExEnd
@@ -57,15 +64,17 @@ namespace QA_Tests.Examples.Tab
         {
             //ExStart
             //ExFor:TabStopCollection.RemoveByIndex
-            //ExSummary:Shows how to select a TabStop in a document by it's index and remove it.
+            //ExSummary:Shows how to select a tab stop in a document by it's index and remove it.
             Aspose.Words.Document doc = new Aspose.Words.Document(ExDir + "Document.doc");
             Aspose.Words.Paragraph paragraph = (Aspose.Words.Paragraph)doc.GetChild(Aspose.Words.NodeType.Paragraph, 0, true);
 
-            paragraph.ParagraphFormat.TabStops.Add(84.99, Aspose.Words.TabAlignment.Left, Aspose.Words.TabLeader.Dashes);
-            paragraph.ParagraphFormat.TabStops.Add(169.98, Aspose.Words.TabAlignment.Left, Aspose.Words.TabLeader.Dashes);
+            paragraph.ParagraphFormat.TabStops.Add(Aspose.Words.ConvertUtil.MillimeterToPoint(30), Aspose.Words.TabAlignment.Left, Aspose.Words.TabLeader.Dashes);
+            paragraph.ParagraphFormat.TabStops.Add(Aspose.Words.ConvertUtil.MillimeterToPoint(60), Aspose.Words.TabAlignment.Left, Aspose.Words.TabLeader.Dashes);
 
-            // TabStop at 84.99 points is removed
+            // Tab stop placed at 30 mm is removed
             paragraph.ParagraphFormat.TabStops.RemoveByIndex(0);
+
+            Console.WriteLine(paragraph.ParagraphFormat.TabStops.Count);
 
             doc.Save(ExDir + "Document.RemovedTabStopsByIndex Out.doc");
             //ExEnd
@@ -76,14 +85,31 @@ namespace QA_Tests.Examples.Tab
         {
             //ExStart
             //ExFor:TabStopCollection.GetPositionByIndex
-            //ExSummary:Shows how to find a tab by it's index and get its position.
+            //ExSummary:Shows how to find a tab stop by it's index and get its position.
             Aspose.Words.Document doc = new Aspose.Words.Document(ExDir + "Document.doc");
             Aspose.Words.Paragraph paragraph = (Aspose.Words.Paragraph)doc.GetChild(Aspose.Words.NodeType.Paragraph, 0, true);
 
-            paragraph.ParagraphFormat.TabStops.Add(85, Aspose.Words.TabAlignment.Left, Aspose.Words.TabLeader.Dashes);
-            paragraph.ParagraphFormat.TabStops.Add(170, Aspose.Words.TabAlignment.Left, Aspose.Words.TabLeader.Dashes);
+            paragraph.ParagraphFormat.TabStops.Add(Aspose.Words.ConvertUtil.MillimeterToPoint(30), Aspose.Words.TabAlignment.Left, Aspose.Words.TabLeader.Dashes);
+            paragraph.ParagraphFormat.TabStops.Add(Aspose.Words.ConvertUtil.MillimeterToPoint(60), Aspose.Words.TabAlignment.Left, Aspose.Words.TabLeader.Dashes);
 
-            Console.WriteLine(paragraph.ParagraphFormat.TabStops.GetPositionByIndex(1)); // 170
+            Console.WriteLine("Tab stop at index {0} of the first paragraph is at {1} points.", 1, paragraph.ParagraphFormat.TabStops.GetPositionByIndex(1));
+            //ExEnd
+        }
+
+        [Test]
+        public void GetIndexByPositionEx()
+        {
+            //ExStart
+            //ExFor:TabStopCollection.GetIndexByPosition
+            //ExSummary:Shows how to look up a position to see if a tab stop exists there, and if so, obtain its index.
+            Aspose.Words.Document doc = new Aspose.Words.Document(ExDir + "Document.doc");
+            Aspose.Words.Paragraph paragraph = (Aspose.Words.Paragraph)doc.GetChild(Aspose.Words.NodeType.Paragraph, 0, true);
+
+            paragraph.ParagraphFormat.TabStops.Add(Aspose.Words.ConvertUtil.MillimeterToPoint(30), Aspose.Words.TabAlignment.Left, Aspose.Words.TabLeader.Dashes);
+
+            // An output of -1 signifies that there is no tab stop at that position.
+            Console.WriteLine(paragraph.ParagraphFormat.TabStops.GetIndexByPosition(Aspose.Words.ConvertUtil.MillimeterToPoint(30))); // 0
+            Console.WriteLine(paragraph.ParagraphFormat.TabStops.GetIndexByPosition(Aspose.Words.ConvertUtil.MillimeterToPoint(60))); // -1
             //ExEnd
         }
     }
