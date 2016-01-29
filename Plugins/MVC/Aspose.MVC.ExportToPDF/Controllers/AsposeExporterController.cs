c0d418e41c02694161d23e59ffe4a499f46d5eaf
Assad Mahmood <assadvirgo@gmail.com>
2016-01-29 13:01:01 +0500

    Aspose.MVC.ExortToPDF v1.0

---
+++ Plugins/MVC/Aspose.MVC.ExportToPDF/Controllers/AsposeExporterController.cs
@@ -0,0 +1,111 @@
+﻿using Aspose.Words;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aspose.MVC.ExportToPDF.Controllers
+{
+    public class AsposeExporterController : Controller
+    {
+        //
+        // GET: /AsposeExporter/
+
+        public void Index(string Format="pdf")
+        {
+
+            string baseURL = Request.Url.Authority;
+
+            if (Request.ServerVariables["HTTPS"] == "on")
+            {
+                baseURL = "https://" + baseURL;
+            }
+            else
+            {
+                baseURL = "http://" + baseURL;
+            }
+
+            // Check for license and apply if exists
+            string licenseFile = Server.MapPath("~/App_Data/Aspose.Words.lic");
+            if (System.IO.File.Exists(licenseFile))
+            {
+                License license = new License();
+                license.SetLicense(licenseFile);
+            }
+
+            string refURL = Request.UrlReferrer.AbsoluteUri;
+
+            string html = new WebClient().DownloadString(refURL);
+
+            // To make the relative image paths work, base URL must be included in head section
+            html = html.Replace("</head>", string.Format("<base href='{0}'></base></head>", baseURL));
+
+            MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(html));
+            Document doc = new Document(stream);
+            string fileName = System.Guid.NewGuid().ToString() + "." + Format;
+            doc.Save(System.Web.HttpContext.Current.Response, fileName, ContentDisposition.Inline, null);
+
+            System.Web.HttpContext.Current.Response.End();
+        }
+
+        public void pdf()
+        {
+            Index("pdf");
+        }
+
+        public void docx()
+        {
+            Index("docx");
+        }
+
+        public void doc()
+        {
+            Index("doc");
+        }
+
+        public void dot()
+        {
+            Index("dot");
+        }
+
+        public void dotx()
+        {
+            Index("dotx");
+        }
+
+        public void docm()
+        {
+            Index("docm");
+        }
+
+        public void dotm()
+        {
+            Index("dotm");
+        }
+
+        public void odt()
+        {
+            Index("odt");
+        }
+
+        public void ott()
+        {
+            Index("ott");
+        }
+
+        public void rtf()
+        {
+            Index("rtf");
+        }
+
+        public void txt()
+        {
+            Index("txt");
+        }
+
+    }
+}
---
+++ Plugins/MVC/Aspose.MVC.ExportToPDF/Readme.md
@@ -0,0 +1,20 @@
+## Export to PDF in ASP.NET MVC using Aspose.Words .NET
+This simplest solution allows developers to add “Export to PDF” and Export to any supported format (DOCX,DOC,DOT,DOTX,DOCM,DOTM,ODT,OTT,RTF,TXT) feature to any of their ASP.NET MVC project in few easy steps. It generates selected format version of any html page.
+
+### Test running demonstration
+
+* Step 1 – Download Aspose.MVC.ExportToPDF Project.
+* Step 2 – Open Aspose.MVC.ExportToPDF Project
+* Step 3 – Resolve “References” and run the project
+
+### Embed “Export to PDF” to an Existing Project
+
+* Step 1 – Download Aspose.MVC.ExportToPDF Project.
+* Step 2 – Open your existing ASP.NET MVC Solution / Project.
+* Step 3 – Copy “Controllers/AsposeExporterController.cs” to your projects “Controllers” folder.
+* Step 4 - Copy “Views/Shared/AsposeWordsExportTypes.cshtml” to your projects “Views/Shared” folder.
+* Step 5 – Add partial view "AsposeWordsExportTypes.cshtml" in your desired view.
+e.g @{ Html.RenderPartial("~/Views/Shared/AsposeWordsExportTypes.cshtml"); }
+
+
+Please download the latest version of Words for .NET from http://www.aspose.com/community/files/default.aspx
