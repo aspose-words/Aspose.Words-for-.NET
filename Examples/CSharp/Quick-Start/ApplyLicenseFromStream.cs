577670a3210e0616198734b742f515f8ff0d5f7c
Tahir Manzoor <tahir.manzoor@aspose.com>
2017-01-25 12:08:41 +0500

    How to Apply License

---
+++ Examples/CSharp/Quick-Start/ApplyLicenseFromStream.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CSharp.Quick_Start
+{
+    class ApplyLicenseFromStream
+    {
+        public static void Run()
+        {
+            //ExStart:ApplyLicenseFromStream
+            Aspose.Words.License license = new Aspose.Words.License();
+
+            try
+            {
+                // Initializes a license from a stream 
+                MemoryStream stream = new MemoryStream(File.ReadAllBytes(@"Aspose.Words.lic"));
+                license.SetLicense(stream);
+                Console.WriteLine("License set successfully.");
+            }
+            catch (Exception e)
+            {
+                // We do not ship any license with this example, visit the Aspose site to obtain either a temporary or permanent license. 
+                Console.WriteLine("\nThere was an error setting the license: " + e.Message);
+            }
+            //ExEnd:ApplyLicenseFromStream
+        }
+    }
+}
