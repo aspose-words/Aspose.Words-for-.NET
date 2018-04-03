6792814f628c6dd7c434d786500474f2f3298a29
romans91 <to.ru@Live.com>
2018-04-04 03:26:23 +1200

    Created branch

---
+++ ApiExamples/CSharp/ExAbsolutePositionTab.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Aspose.Words;
+
+namespace ApiExamples
+{
+    class ExAbsolutePositionTab : ApiExampleBase
+    {
+        [Test]
+        public void AbsolutePositionTab()
+        {
+            //ExStart
+            //ExFor:ExAbsolutePositionTab
+            //ExId:AbsolutePositionTab
+            //ExSummary:Shows how to use AbsolutePositionTab.
+            Document doc = new Document(MyDir + "AbsolutePositionTab.doc");
+
+            //ExEnd
+        }
+    }
+}
