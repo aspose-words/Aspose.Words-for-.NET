8c624ae4b2e40511f6ed7f2423e921885634c0b4
tahir-manzoor <tahir.manzoor@aspose.com>
2018-04-18 15:24:57 +0500

    FindAndReplace using MetaCharacters

--- Examples/CSharp/Programming-Documents/Find-Replace/FindReplaceUsingMetaCharacters.cs
+++ Examples/CSharp/Programming-Documents/Find-Replace/FindReplaceUsingMetaCharacters.cs
@@ -12,15 +12,21 @@ namespace Aspose.Words.Examples.CSharp.Programming_Documents.Find_and_Replace
         public static void Run()
         {
             /* meta-characters
-            &p - paragraph break
-            &b - section break
-            &m - page break
-            &l - manual line break
-                */
-            // ExStart:FindReplaceUsingMetaCharacters
+           &p - paragraph break
+           &b - section break
+           &m - page break
+           &l - manual line break
+               */
+
             // The path to the documents directory.
             string dataDir = RunExamples.GetDataDir_FindAndReplace();
+            MetaCharactersInSearchPattern(dataDir);
+            ReplaceTextContaingMetaCharacters(dataDir);
+        }
+        public static void MetaCharactersInSearchPattern(string dataDir)
+        {
 
+            // ExStart:MetaCharactersInSearchPattern
             // Initialize a Document.
             Document doc = new Document();
 
@@ -29,7 +35,7 @@ namespace Aspose.Words.Examples.CSharp.Programming_Documents.Find_and_Replace
             builder.Writeln("This is Line 1");
             builder.Writeln("This is Line 2");
 
-            var findReplaceOptions = new FindReplaceOptions(); 
+            var findReplaceOptions = new FindReplaceOptions();
 
             doc.Range.Replace("This is Line 1&pThis is Line 2", "This is replaced line", findReplaceOptions);
 
@@ -40,10 +46,40 @@ namespace Aspose.Words.Examples.CSharp.Programming_Documents.Find_and_Replace
 
             doc.Range.Replace("This is Line 1&mThis is Line 2", "Page break is replaced with new text.", findReplaceOptions);
 
-            dataDir = dataDir + "FindReplaceUsingMetaCharacters_out.docx";
+            dataDir = dataDir + "MetaCharactersInSearchPattern_out.docx";
             doc.Save(dataDir);
-            // ExEnd:FindReplaceUsingMetaCharacters
+            // ExEnd:MetaCharactersInSearchPattern
             Console.WriteLine("\nFind and Replace text using meta-characters has done successfully.\nFile saved at " + dataDir);
+        }
+
+        public static void ReplaceTextContaingMetaCharacters(string dataDir)
+        {
+            // ExStart:ReplaceTextContaingMetaCharacters
+            Document doc = new Document();
+            DocumentBuilder builder = new DocumentBuilder(doc);
+
+            builder.Font.Name = "Arial";
+            builder.Writeln("First section");
+            builder.Writeln("  1st paragraph");
+            builder.Writeln("  2nd paragraph");
+            builder.Writeln("{insert-section}");
+            builder.Writeln("Second section");
+            builder.Writeln("  1st paragraph");
+
+            FindReplaceOptions options = new FindReplaceOptions();
+            options.ApplyParagraphFormat.Alignment = ParagraphAlignment.Center;
+
+            // Double each paragraph break after word "section", add kind of underline and make it centered.
+            int count = doc.Range.Replace("section&p", "section&p----------------------&p", options);
+
+            // Insert section break instead of custom text tag.
+            count = doc.Range.Replace("{insert-section}", "&b", options);
+
+            dataDir = dataDir + "ReplaceTextContaingMetaCharacters_out.docx";
+            doc.Save(dataDir);
+            // ExEnd:ReplaceTextContaingMetaCharacters
+            Console.WriteLine("\nFind and Replace text using meta-characters has done successfully.\nFile saved at " + dataDir);
+
         }
     }
 }
