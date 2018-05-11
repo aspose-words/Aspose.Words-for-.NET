2afa8a6428bda79a3ce99a72d37f5b8c65ee91c1
romans91 <to.ru@Live.com>
2018-05-12 01:27:56 +1200

    Created branch

---
+++ ApiExamples/CSharp/ExDocumentBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ApiExamples
+{
+    [TestFixture]
+    public class ExDocumentBase : ApiExampleBase
+    {
+        [Test]
+        public void DocumentBaseMisc()
+        {
+            //ExFor:DocumentBase
+            //ExFor:DocumentBase.BackgroundShape
+            //ExFor:DocumentBase.ImportNode(Node,System.Boolean)
+            //ExFor:DocumentBase.ImportNode(Node,System.Boolean,ImportFormatMode)
+            //ExFor:DocumentBase.ImportNode(Node,System.Boolean,ImportFormatMode,INodeCloningListener)
+            //ExFor:DocumentBase.ImportNode(Node,System.Boolean,INodeCloningListener)
+            //ExFor:DocumentBase.PageColor
+            //ExFor:DocumentBase.ResourceLoadingCallback
+            //ExFor:DocumentBase.WarningCallback
+        }
+    }
+}
