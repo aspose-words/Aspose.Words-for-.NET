e5b7d1e54e82f3e7565b830c8cdd61ed48a673f4
romans91 <to.ru@Live.com>
2018-11-15 20:29:18 +1300

    All examples

--- ApiExamples/CSharp/ApiExamples/ExField.cs
+++ ApiExamples/CSharp/ApiExamples/ExField.cs
@@ -3206,5 +3206,34 @@ namespace ApiExamples
             Assert.AreEqual("0", field.Result);
             //ExEnd
         }
+
+        [Test]
+        public void FieldGoToButton()
+        {
+            //ExStart
+            //ExFor:FieldGoToButton
+            //ExFor:FieldGoToButton.DisplayText
+            //ExFor:FieldGoToButton.Location
+            //ExSummary:Shows to insert a GOTOBUTTON field.
+            Document doc = new Document();
+            DocumentBuilder builder = new DocumentBuilder(doc);
+
+            // Add a GOTOBUTTON which will take us to a bookmark referenced by "MyBookmark"
+            FieldGoToButton field = (FieldGoToButton)builder.InsertField(FieldType.FieldGoToButton, true);
+            field.DisplayText = "My Button";
+            field.Location = "MyBookmark";
+
+            Assert.AreEqual(" GOTOBUTTON  MyBookmark My Button", field.GetFieldCode());
+
+            // Add an arrival destination for our button
+            builder.InsertBreak(BreakType.PageBreak);
+            builder.StartBookmark(field.Location);
+            builder.Writeln("Bookmark text contents.");
+            builder.EndBookmark(field.Location);
+
+            doc.UpdateFields();
+            doc.Save(MyDir + @"\Artifacts\Field.GoToButton.docx");
+            //ExEnd
+        }
     }
 }
