e59a15fcb187fad214a320663123fe013d1fd920
Aspose.Words Product Family <words@aspose.com>
2018-11-17 15:23:12 +0500

    Updated according to WORDSNET-17595
    Some corrections to Common.cs class have been made.

--- Examples/CSharp/Programming-Documents/Document/Common.cs
+++ Examples/CSharp/Programming-Documents/Document/Common.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 
 using Aspose.Words;
@@ -17,17 +18,23 @@ namespace Aspose.Words.Examples.CSharp.Programming_Documents.Working_With_Docume
             // Create a list to store the extracted nodes.
             ArrayList nodes = new ArrayList();
 
+            // If either marker is part of a comment then to include the comment itself we need to move the pointer
+            // forward to the Comment Node found after the CommentRangeEnd node.
+            if (endNode.NodeType == NodeType.CommentRangeEnd && isInclusive)
+            {
+                Node node = FindNextNode(NodeType.Comment, endNode.NextSibling);
+                if (node != null)
+                    endNode = node;
+            }
+
             // Keep a record of the original nodes passed to this method so we can split marker nodes if needed.
             Node originalStartNode = startNode;
             Node originalEndNode = endNode;
 
             // Extract content based on block level nodes (paragraphs and tables). Traverse through parent nodes to find them.
             // We will split the content of first and last nodes depending if the marker nodes are inline
-            while (startNode.ParentNode.NodeType != NodeType.Body)
-                startNode = startNode.ParentNode;
-
-            while (endNode.ParentNode.NodeType != NodeType.Body)
-                endNode = endNode.ParentNode;
+            startNode = GetAncestorInBody(startNode);
+            endNode = GetAncestorInBody(endNode);
 
             bool isExtracting = true;
             bool isStartingNode = true;
@@ -43,20 +50,24 @@ namespace Aspose.Words.Examples.CSharp.Programming_Documents.Working_With_Docume
                 Node cloneNode = currNode.Clone(true);
                 isEndingNode = currNode.Equals(endNode);
 
-                if ((isStartingNode || isEndingNode) && cloneNode.IsComposite)
+                if (isStartingNode || isEndingNode)
                 {
                     // We need to process each marker separately so pass it off to a separate method instead.
-                    if (isStartingNode)
+                    // End should be processed at first to keep node indexes.
+                    if (isEndingNode)
                     {
-                        ProcessMarker((CompositeNode)cloneNode, nodes, originalStartNode, isInclusive, isStartingNode, isEndingNode);
-                        isStartingNode = false;
+                        // !isStartingNode: don't add the node twice if the markers are the same node.
+                        ProcessMarker(cloneNode, nodes, originalEndNode, currNode, isInclusive,
+                            false, !isStartingNode, false);
+                        isExtracting = false;
                     }
 
                     // Conditional needs to be separate as the block level start and end markers maybe the same node.
-                    if (isEndingNode)
+                    if (isStartingNode)
                     {
-                        ProcessMarker((CompositeNode)cloneNode, nodes, originalEndNode, isInclusive, isStartingNode, isEndingNode);
-                        isExtracting = false;
+                        ProcessMarker(cloneNode, nodes, originalStartNode, currNode, isInclusive,
+                            true, true, false);
+                        isStartingNode = false;
                     }
                 }
                 else
@@ -77,6 +88,10 @@ namespace Aspose.Words.Examples.CSharp.Programming_Documents.Working_With_Docume
                 }
             }
 
+            // For compatibility with mode with inline bookmarks, add the next paragraph (empty).
+            if (isInclusive && originalEndNode == endNode && !originalEndNode.IsComposite)
+                IncludeNextParagraph(endNode, nodes);
+
             // Return the nodes between the node markers.
             return nodes;
         }
@@ -96,6 +111,7 @@ namespace Aspose.Words.Examples.CSharp.Programming_Documents.Working_With_Docume
             }
             return paragraphsWithStyle;
         }
+
         // ExStart:CommonGenerateDocument
         public static Document GenerateDocument(Document srcDoc, ArrayList nodes)
         {
@@ -116,6 +132,7 @@ namespace Aspose.Words.Examples.CSharp.Programming_Documents.Working_With_Docume
             // Return the generated document.
             return dstDoc;
         }
+
         // ExEnd:CommonGenerateDocument  
         // ExStart:CommonExtractContentHelperMethods
         private static void VerifyParameterNodes(Node startNode, Node endNode)
@@ -142,30 +159,48 @@ namespace Aspose.Words.Examples.CSharp.Programming_Documents.Working_With_Docume
 
             if (startIndex == endIndex)
             {
-                if (startSection.Body.IndexOf(startNode) > endSection.Body.IndexOf(endNode))
+                if (startSection.Body.IndexOf(GetAncestorInBody(startNode)) >
+                        endSection.Body.IndexOf(GetAncestorInBody(endNode)))
                     throw new ArgumentException("The end node must be after the start node in the body");
             }
             else if (startIndex > endIndex)
                 throw new ArgumentException("The section of end node must be after the section start node");
         }
+
+        private static Node FindNextNode(NodeType nodeType, Node fromNode)
+        {
+            if (fromNode == null || fromNode.NodeType == nodeType)
+                return fromNode;
+
+            if (fromNode.IsComposite)
+            {
+                Node node = FindNextNode(nodeType, ((CompositeNode)fromNode).FirstChild);
+                if (node != null)
+                    return node;
+            }
+
+            return FindNextNode(nodeType, fromNode.NextSibling);
+        }
+
         private static bool IsInline(Node node)
         {
             // Test if the node is desendant of a Paragraph or Table node and also is not a paragraph or a table a paragraph inside a comment class which is decesant of a pararaph is possible.
             return ((node.GetAncestor(NodeType.Paragraph) != null || node.GetAncestor(NodeType.Table) != null) && !(node.NodeType == NodeType.Paragraph || node.NodeType == NodeType.Table));
         }
-        private static void ProcessMarker(CompositeNode cloneNode, ArrayList nodes, Node node, bool isInclusive, bool isStartMarker, bool isEndMarker)
+
+        private static void ProcessMarker(Node cloneNode, ArrayList nodes, Node node, Node blockLevelAncestor,
+            bool isInclusive, bool isStartMarker, bool canAdd, bool forceAdd)
         {
             // If we are dealing with a block level node just see if it should be included and add it to the list.
-            if (!IsInline(node))
+            if (node == blockLevelAncestor)
             {
-                // Don't add the node twice if the markers are the same node
-                if (!(isStartMarker && isEndMarker))
-                {
-                    if (isInclusive)
-                        nodes.Add(cloneNode);
-                }
+                if (canAdd && isInclusive)
+                    nodes.Add(cloneNode);
                 return;
             }
+            // cloneNode is a clone of blockLevelNode. If node != blockLevelNode, blockLevelAncestor is ancestor of node
+            // that means it is a composite node.
+            Debug.Assert(cloneNode.IsComposite);
 
             // If a marker is a FieldStart node check if it's to be included or not.
             // We assume for simplicity that the FieldStart and FieldEnd appear in the same paragraph.
@@ -177,42 +212,43 @@ namespace Aspose.Words.Examples.CSharp.Programming_Documents.Working_With_Docume
                 {
                     while (node.NextSibling != null && node.NodeType != NodeType.FieldEnd)
                         node = node.NextSibling;
-
                 }
             }
 
-            // If either marker is part of a comment then to include the comment itself we need to move the pointer forward to the Comment
-            // Node found after the CommentRangeEnd node.
-            if (node.NodeType == NodeType.CommentRangeEnd)
-            {
-                while (node.NextSibling != null && node.NodeType != NodeType.Comment)
-                    node = node.NextSibling;
+            // Support a case if marker node is on the third level of document body or lower.
+            List<Node> nodeBranch = FillSelfAndParents(node, blockLevelAncestor);
 
+            // Process the corresponding node in our cloned node by index.
+            Node currentCloneNode = cloneNode;
+            for (int i = nodeBranch.Count - 1; i >= 0; i--)
+            {
+                Node currentNode = nodeBranch[i];
+                int nodeIndex = currentNode.ParentNode.IndexOf(currentNode);
+                currentCloneNode = ((CompositeNode)currentCloneNode).ChildNodes[nodeIndex];
+
+                RemoveNodesOutsideOfRange(currentCloneNode, isInclusive || (i > 0), isStartMarker);
             }
 
-            // Find the corresponding node in our cloned node by index and return it.
-            // If the start and end node are the same some child nodes might already have been removed. Subtract the
-            // Difference to get the right index.
-            int indexDiff = node.ParentNode.ChildNodes.Count - cloneNode.ChildNodes.Count;
+            // After processing the composite node may become empty. If it has don't include it.
+            if (canAdd &&
+                (forceAdd || ((CompositeNode)cloneNode).HasChildNodes))
+                nodes.Add(cloneNode);
+        }
 
-            // Child node count identical.
-            if (indexDiff == 0)
-                node = cloneNode.ChildNodes[node.ParentNode.IndexOf(node)];
-            else
-                node = cloneNode.ChildNodes[node.ParentNode.IndexOf(node) - indexDiff];
-
+        private static void RemoveNodesOutsideOfRange(Node markerNode, bool isInclusive, bool isStartMarker)
+        {
             // Remove the nodes up to/from the marker.
             bool isSkip = false;
             bool isProcessing = true;
             bool isRemoving = isStartMarker;
-            Node nextNode = cloneNode.FirstChild;
+            Node nextNode = markerNode.ParentNode.FirstChild;
 
             while (isProcessing && nextNode != null)
             {
                 Node currentNode = nextNode;
                 isSkip = false;
 
-                if (currentNode.Equals(node))
+                if (currentNode.Equals(markerNode))
                 {
                     if (isStartMarker)
                     {
@@ -232,14 +268,41 @@ namespace Aspose.Words.Examples.CSharp.Programming_Documents.Working_With_Docume
                 if (isRemoving && !isSkip)
                     currentNode.Remove();
             }
+        }
 
-            // After processing the composite node may become empty. If it has don't include it.
-            if (!(isStartMarker && isEndMarker))
+        private static List<Node> FillSelfAndParents(Node node, Node tillNode)
+        {
+            List<Node> list = new List<Node>();
+            Node currentNode = node;
+
+            while (currentNode != tillNode)
             {
-                if (cloneNode.HasChildNodes)
-                    nodes.Add(cloneNode);
+                list.Add(currentNode);
+                currentNode = currentNode.ParentNode;
             }
 
+            return list;
+        }
+
+        private static void IncludeNextParagraph(Node node, ArrayList nodes)
+        {
+            Paragraph paragraph = (Paragraph)FindNextNode(NodeType.Paragraph, node.NextSibling);
+            if (paragraph != null)
+            {
+                // Move to first child to include paragraph without contents.
+                Node markerNode = paragraph.HasChildNodes ? paragraph.FirstChild : paragraph;
+                Node rootNode = GetAncestorInBody(paragraph);
+
+                ProcessMarker(rootNode.Clone(true), nodes, markerNode, rootNode,
+                    markerNode == paragraph, false, true, true);
+            }
+        }
+
+        private static Node GetAncestorInBody(Node startNode)
+        {
+            while (startNode.ParentNode.NodeType != NodeType.Body)
+                startNode = startNode.ParentNode;
+            return startNode;
         }
         // ExEnd:CommonExtractContentHelperMethods
     }
