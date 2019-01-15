c7a6b318a65cc4392383e00572f568f8141e81ee
tahir-manzoor <tahir.manzoor@aspose.com>
2019-01-15 11:32:06 +0500

    DocSaveOptions

---
+++ Examples/CSharp/Loading-and-Saving/WorkingWithDoc.cs
@@ -0,0 +1,32 @@
+﻿using Aspose.Words.Saving;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspose.Words.Examples.CSharp.Loading_Saving
+{
+    class WorkingWithDoc
+    {
+        public static void Run()
+        {
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_LoadingAndSaving();
+
+            EncryptDocumentWithPassword(dataDir);
+        }
+
+        public static void EncryptDocumentWithPassword(string dataDir)
+        {
+            //ExStart:EncryptDocumentWithPassword
+            Document doc = new Document(dataDir + "Document.doc");
+            DocSaveOptions docSaveOptions = new DocSaveOptions();
+            docSaveOptions.Password = "password";
+            dataDir = dataDir + "Document.Password_out.doc";
+            doc.Save(dataDir);
+            //ExEnd:EncryptDocumentWithPassword
+            Console.WriteLine("\nThe password of document is set using RC4 encryption method. \nFile saved at " + dataDir);
+        }
+    }
+}
