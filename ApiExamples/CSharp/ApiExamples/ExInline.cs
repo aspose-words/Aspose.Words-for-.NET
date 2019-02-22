98b39e74bfc5ed6bc7a6f327e5c4f9d14be09428
romans91 <to.ru@Live.com>
2019-02-23 02:48:00 +1300

    Created class

---
+++ ApiExamples/CSharp/ApiExamples/ExInline.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) 2001-2019 Aspose Pty Ltd. All Rights Reserved.
+//
+// This file is part of Aspose.Words. The source code in this file
+// is only intended as a supplement to the documentation, and is provided
+// "as is", without warranty of any kind, either expressed or implied.
+//////////////////////////////////////////////////////////////////////////
+
+using NUnit.Framework;
+
+namespace ApiExamples
+{
+    [TestFixture]
+    class ExInline : ApiExampleBase
+    {
+        [Test]
+        public void Inline()
+        {
+            //ExStart
+            //ExFor:Inline
+            //ExFor:Inline.IsDeleteRevision
+            //ExFor:Inline.IsFormatRevision
+            //ExFor:Inline.IsInsertRevision
+            //ExFor:Inline.IsMoveFromRevision
+            //ExFor:Inline.IsMoveToRevision
+            //ExFor:Inline.ParentParagraph
+            //ExSummary:Shows how to add a footnote to a paragraph in the document.
+        }
+    }
+}
