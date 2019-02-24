e2b65c1479345f6ac4b0ec9131cd705912c1c5f5
romans91 <to.ru@Live.com>
2019-02-25 02:58:51 +1300

    All examples

--- ApiExamples/CSharp/ApiExamples/ExInline.cs
+++ ApiExamples/CSharp/ApiExamples/ExInline.cs
@@ -25,20 +25,34 @@ namespace ApiExamples
             //ExFor:Inline.IsMoveFromRevision
             //ExFor:Inline.IsMoveToRevision
             //ExFor:Inline.ParentParagraph
-            //ExSummary:.
-            Document doc = new Document();
-            DocumentBuilder builder = new DocumentBuilder(doc);
+            //ExSummary:Shows how to view revision-related properties of Inline nodes.
+            Document doc = new Document(MyDir + "Inline.Revisions.docx");
 
-            builder.Write("Is not a revision");
+            // This document has 5 revisions
+            Assert.AreEqual(5, doc.Revisions.Count);
 
-            Assert.False(doc.HasRevisions);
-            Assert.False(builder.CurrentParagraph.Runs[0].IsInsertRevision);
+            // The parent node of a revision is the run that the revision concerns, which is an Inline node
+            Run run = (Run)doc.Revisions[0].ParentNode;
 
-            doc.StartTrackRevisions("John Doe", DateTime.Now);
+            // Get the parent paragraph
+            Paragraph firstParagraph = run.ParentParagraph;
+            RunCollection runs = firstParagraph.Runs;
 
-            builder.Write("Is a revision");
-            Assert.True(doc.HasRevisions);
-            Assert.True(builder.CurrentParagraph.Runs[1].IsInsertRevision);
+            Assert.AreEqual(6, runs.Count);
+
+            // For all runs not involved in revisions, all the Is...Revision flags will be false
+
+            // The text in the run at index #2 was typed after revisions were tracked, so it will count as an insert revision
+            // The font was changed, so it will also be a format revision
+            Assert.IsTrue(runs[2].IsInsertRevision);
+            Assert.IsTrue(runs[2].IsFormatRevision);
+
+            // For a "move", in regards to tracked revisions, to take place,
+            // some text which contains at least one complete sentence must be removed from one location and placed into another
+            // Typically this will happen when we highlight text with the mouse and drag it around, or cut and paste (but not copy and paste)
+            // The node with the "IsMoveToRevision" flag is the destination, and the node with the "IsMoveFromRevision" flag is the departure point
+            Assert.IsTrue(runs[1].IsMoveToRevision);
+            Assert.IsTrue(runs[4].IsMoveFromRevision);
         }
     }
 }
