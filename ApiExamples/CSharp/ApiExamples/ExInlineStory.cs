7cc30f0db5242d442ee3f9a5da3281575ad41d07
romans91 <to.ru@Live.com>
2019-02-27 20:06:24 +1300

    All examples

--- ApiExamples/CSharp/ApiExamples/ExInlineStory.cs
+++ ApiExamples/CSharp/ApiExamples/ExInlineStory.cs
@@ -6,7 +6,11 @@
 //////////////////////////////////////////////////////////////////////////
 
 using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 using Aspose.Words;
+using Aspose.Words.Tables;
 using NUnit.Framework;
 
 namespace ApiExamples
@@ -60,6 +64,94 @@ namespace ApiExamples
             //ExEnd
 
             Assert.AreEqual("Comment text.\r", (doc.GetChildNodes(NodeType.Comment, true)[0]).GetText());
+        }
+
+        [Test]
+        public void InlineStoryRevisions()
+        {
+            //ExStart
+            //ExFor:InlineStory.IsDeleteRevision
+            //ExFor:InlineStory.IsInsertRevision
+            //ExFor:InlineStory.IsMoveFromRevision
+            //ExFor:InlineStory.IsMoveToRevision
+            //ExSummary:Shows how to work with InlineStory revisions.
+            // Open a document that has revisions from changes being tracked
+            Document doc = new Document(MyDir + "InlineStory.Revisions.docx");
+            Assert.IsTrue(doc.HasRevisions);
+
+            // Get a collection of all footnotes from the document
+            List<Footnote> footnotes = doc.GetChildNodes(NodeType.Footnote, true).Cast<Footnote>().ToList();
+            Assert.AreEqual(4, footnotes.Count);
+
+            // If a node was inserted in Microsoft Word while changes were being tracked, this flag will be set to true
+            Assert.IsTrue(footnotes[2].IsInsertRevision);
+
+            // If one node was moved from one place to another while changes were tracked,
+            // the node will be placed at the departure location as a "move to revision",
+            // and a "move from revision" node will be left behind at the origin, in case we want to reject changes
+            // Highlighting text and dragging it to another place with the mouse and cut-and-pasting (but not copy-pasting) both count as "move revisions"
+            // The node with the "IsMoveToRevision" flag is the arrival of the move operation, and the node with the "IsMoveFromRevision" flag is the departure point
+            Assert.IsTrue(footnotes[1].IsMoveToRevision);
+            Assert.IsTrue(footnotes[3].IsMoveFromRevision);
+        }
+
+        [Test]
+        public void InsertInlineStoryNodes()
+        {
+            //ExStart
+            //ExFor:InlineStory.EnsureMinimum
+            //ExFor:InlineStory.Font
+            //ExFor:InlineStory.LastParagraph
+            //ExFor:InlineStory.ParentParagraph
+            //ExFor:InlineStory.StoryType
+            //ExFor:InlineStory.Tables
+            //ExSummary:Shows how to insert InlineStory nodes.
+            // Create a new document and insert a blank footnote
+            Document doc = new Document();
+            DocumentBuilder builder = new DocumentBuilder(doc);
+            Footnote footnote = builder.InsertFootnote(FootnoteType.Footnote, null);
+
+            // Table nodes have an "EnsureMinimum()" method that makes sure the table has at least one cell
+            Table table = new Table(doc);
+            table.EnsureMinimum();
+
+            // We can place a table inside a footnote, which will make it appear at the footer of the referencing page
+            Assert.IsEmpty(footnote.Tables);
+            footnote.AppendChild(table);
+            Assert.AreEqual(1, footnote.Tables.Count);
+            Assert.AreEqual(NodeType.Table, footnote.LastChild.NodeType);
+
+            // An InlineStory has an "EnsureMinimum()" method as well, but in this case it makes sure the last child of the node is a paragraph,
+            // so we can click and write text easily in Microsoft Word
+            footnote.EnsureMinimum();
+            Assert.AreEqual(NodeType.Paragraph, footnote.LastChild.NodeType);
+
+            // Edit the appearance of the anchor, which is the small superscript number in the main text that points to the footnote
+            footnote.Font.Name = "Arial";
+            footnote.Font.Color = Color.Green;
+
+            // All inline story nodes have their own respective story types
+            Assert.AreEqual(StoryType.Footnotes, footnote.StoryType);
+
+            // A comment is another type of inline story
+            Comment comment = (Comment)builder.CurrentParagraph.AppendChild(new Comment(doc, "John Doe", "J. D.", DateTime.Now));
+
+            // The parent paragraph of an inline story node will be the one from the main document body
+            Assert.AreEqual(doc.FirstSection.Body.FirstParagraph, comment.ParentParagraph);
+
+            // However, the last paragraph is the one from the comment text contents, which will be outside the main document body in a speech bubble
+            // A comment won't have any child nodes by default, so we can apply the EnsureMinimum() method to place a paragraph here as well
+            Assert.Null(comment.LastParagraph);
+            comment.EnsureMinimum();
+            Assert.AreEqual(NodeType.Paragraph, comment.LastChild.NodeType);
+
+            // Once we have a paragraph, we can move the builder do it and write our comment
+            builder.MoveTo(comment.LastParagraph);
+            builder.Write("My comment");
+
+            Assert.AreEqual(StoryType.Comments, comment.StoryType);
+
+            doc.Save(ArtifactsDir + "Document.InlineStory.docx");
         }
     }
 }
