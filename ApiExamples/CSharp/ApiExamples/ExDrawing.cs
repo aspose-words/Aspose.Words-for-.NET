f0e0da414c7a0c3f7d4c3c74f08ad117cc1350b3
romans91 <to.ru@Live.com>
2019-03-15 02:57:19 +1300

    Wording

--- ApiExamples/CSharp/ApiExamples/ExDrawing.cs
+++ ApiExamples/CSharp/ApiExamples/ExDrawing.cs
@@ -423,7 +423,7 @@ namespace ApiExamples
         }
 
         [Test]
-        public void ImageSizeTransform()
+        public void ImageSize()
         {
             //ExStart
             //ExFor:ImageSize.#ctor(ImageSizeCore)
@@ -433,6 +433,7 @@ namespace ApiExamples
             //ExFor:ImageSize.HorizontalResolution
             //ExFor:ImageSize.VerticalResolution
             //ExFor:ImageSize.WidthPixels
+            //ExSummary:Shows how to access and use a shape's ImageSize property.
             Document doc = new Document();
             DocumentBuilder builder = new DocumentBuilder(doc);
 
@@ -442,15 +443,17 @@ namespace ApiExamples
             // If the shape contains an image, its ImageData property will be valid, and it will contain an ImageSize object
             ImageSize imageSize = shape.ImageData.ImageSize;
 
-            // The ImageSize object contains raw information about the image that the shape displays
+            // The ImageSize object contains raw information about the image within the shape
             Assert.AreEqual(200, imageSize.HeightPixels);
             Assert.AreEqual(200, imageSize.WidthPixels);
 
             Assert.AreEqual(95.986599999999996d, imageSize.HorizontalResolution);
             Assert.AreEqual(95.986599999999996d, imageSize.VerticalResolution);
 
-            // Those values are read-only, so if we want to transform the image, we have to change the size of the shape itself
-            // We can those values as a reference, for example, to double the size of the image
+            // These values are read-only
+            // If we want to transform the image, we need to change the size of the shape that contains it
+            // We can still use values within ImageSize as a reference
+            // In the example below, we will get the shape to display the image in twice its original size
             shape.Width = imageSize.WidthPoints * 2;
             shape.Height = imageSize.HeightPoints * 2;
 
