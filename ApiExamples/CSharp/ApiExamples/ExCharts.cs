85ba715e0064a2c1e9a93727ded2af79508ec0bf
romans91 <to.ru@Live.com>
2019-03-31 23:14:11 +1300

    More concise comments

--- ApiExamples/CSharp/ApiExamples/ExCharts.cs
+++ ApiExamples/CSharp/ApiExamples/ExCharts.cs
@@ -69,7 +69,7 @@ namespace ApiExamples
         //ExFor:Charts.ChartDataLabelCollection.GetEnumerator
         //ExFor:Charts.ChartDataLabelCollection.Item(System.Int32)
         //ExFor:Charts.ChartDataLabelCollection.RemoveAt(System.Int32)
-        //ExSummary:Shows how to apply labels to chart data points.
+        //ExSummary:Shows how to apply labels to data points in a chart.
         [Test] //ExSkip
         public void ChartDataLabels()
         {
@@ -117,7 +117,7 @@ namespace ApiExamples
         }
 
         /// <summary>
-        /// Apply uniform data labels to a number, determined by labelsCount, of data points in a series
+        /// Apply uniform data labels to a number (determined by labelsCount) of data points in a series
         /// Also apply a number format and separator
         /// </summary>
         private void ApplyDataLabels(ChartSeries series, int labelsCount, string numberFormat, string separator)
@@ -125,23 +125,27 @@ namespace ApiExamples
             for (int i = 0; i < labelsCount; i++)
             {
                 ChartDataLabel label = series.DataLabels.Add(i);
+                Assert.False(label.IsVisible);
+
+                // Edit the appearance of the new data label
                 label.ShowCategoryName = true;
                 label.ShowSeriesName = true;
                 label.ShowValue = true;
+                label.ShowLeaderLines = true;
+                label.ShowLegendKey = true;
+                label.ShowPercentage = false;
+                Assert.False(label.ShowDataLabelsRange);
 
+                // Apply number format and separator
                 label.NumberFormat.FormatCode = numberFormat;
                 label.Separator = separator;
 
-                label.ShowLeaderLines = true;
-                label.ShowLegendKey = true;
-
-                Assert.False(label.ShowPercentage);
-                Assert.False(label.ShowDataLabelsRange);
-                Assert.True(label.IsVisible);
-
-                // Attributes that only apply to bubble graphs
+                // Attributes that apply exclusively to bubble graphs
                 Assert.False(series.Bubble3D);
                 Assert.False(label.ShowBubbleSize);
+
+                // The label automatically becomes visible
+                Assert.True(label.IsVisible);
             }
         }
         //ExEnd
@@ -165,12 +169,11 @@ namespace ApiExamples
             // Some of the other possible applications are bar, column, line and surface charts
             Chart chart = AppendChart(builder, ChartType.Column, 300, 300);
 
-            // The number of categories and their names are defined by a string array
-            // Each category will be represented by a bar cluster in the chart
+            // Create and name 3 categories with a string array
             string[] categories = { "Category 1", "Category 2", "Category 3" };
 
-            // The chart will have 3 bar clusters and each cluster will have two columns to compare,
-            // side by side, data values of the index of the cluster's category from each series
+            // Create 2 series of data, each with one point for every category
+            // This will generate a column graph with 3 clusters of 2 bars
             chart.Series.Add("Series 1", categories, new [] { 76.6, 82.1, 91.6 });
             chart.Series.Add("Series 2", categories, new [] { 64.2, 79.5, 94.0 });
 
@@ -178,7 +181,7 @@ namespace ApiExamples
             // Area, radar and stock charts are some of the appropriate chart types for this
             chart = AppendChart(builder, ChartType.Area, 300, 300);
 
-            // Create a collection of dates
+            // Create a collection of dates to serve as categories
             DateTime[] dates = { new DateTime(2014, 3, 31),
                 new DateTime(2017, 1, 23),
                 new DateTime(2017, 6, 18),
@@ -186,25 +189,22 @@ namespace ApiExamples
                 new DateTime(2020, 9, 7)
             };
 
-            // Add a series with the collection of dates and an array of data, with a value for each date
-            // Our area chart will adjust for the sporadic DateTime values and display the data along a linear time scale
+            // Add one series with one point for each date
+            // Our sporadic dates will be distributed along the X-axis in a linear fashion 
             chart.Series.Add("Series 1", dates, new [] { 15.8, 21.5, 22.9, 28.7, 33.1 });
 
             // 3: Each series will take two data arrays
             // Appropriate for scatter plots
             chart = AppendChart(builder, ChartType.Scatter, 300, 300);
 
-            // Two arrays are passed, both of the same size
-            // The first array contains X-coordinates and the second contains respective Y-coordinates of the points that will populate the scatter chart
-            // Each series adds 8 points which will share the same color, to differentiate from the points of another series
+            // In each series, the first array contains the X-coordinates and the second contains respective Y-coordinates of points
             chart.Series.Add("Series 1", new[] { 3.1, 3.5, 6.3, 4.1, 2.2, 8.3, 1.2, 3.6 }, new[] { 3.1, 6.3, 4.6, 0.9, 8.5, 4.2, 2.3, 9.9 });
             chart.Series.Add("Series 2", new[] { 2.6, 7.3, 4.5, 6.6, 2.1, 9.3, 0.7, 3.3 }, new[] { 7.1, 6.6, 3.5, 7.8, 7.7, 9.5, 1.3, 4.6 });
 
             // 4: Each series will be built from three data arrays, used for bubble charts
             chart = AppendChart(builder, ChartType.Bubble, 300, 300);
 
-            // The first two passed data arrays are X and Y coordinates, like in the scatter plot above
-            // The third data array specifies the displayed thickness of each point
+            // The first two arrays contain X/Y coordinates like above and the third determines the thickness of each point
             chart.Series.Add("Series 1", new [] { 1.1, 5.0, 9.8 }, new [] { 1.2, 4.9, 9.9 }, new [] { 2.0, 4.0, 8.0 });
 
             doc.Save(ArtifactsDir + "Charts.ChartSeriesCollection.docx");
@@ -243,20 +243,16 @@ namespace ApiExamples
             chart.Series.Clear();
 
             // Insert a series with X/Y coordinates for 5 points
-            // Note that, because of the erratic Y values, a graph with linear Y-axis scaling may not produce a satisfactory result
-            chart.Series.Add("Series 1", new[] { 1.0, 2.0, 3.0, 4.0, 5.0 }, new[] { 1.0, 10.0, 100.0, 1000.0, 10000.0 });
+            chart.Series.Add("Series 1", new[] { 1.0, 2.0, 3.0, 4.0, 5.0 }, new[] { 1.0, 20.0, 400.0, 8000.0, 160000.0 });
 
-            // The scaling of the X axis is linear,
-            // which means that the X value goes up by the same amount (in this case, 1) at each vertical line
-            // This X-axis ruler will display "1  2  3.."
+            // The scaling of the X axis is linear by default, which means it will display "0, 1, 2, 3..."
             Assert.AreEqual(AxisScaleType.Linear, chart.AxisX.Scaling.Type);
 
-            // As for the Y axis, we can set the scaling to Logarithmic,
-            // which means that the Y-values go up by an order of magnitude at each horizontal line
-            // The order of magnitude is decided by the LogBase attribute, which we will leave at 10, its default value
-            // This Y-axis ruler will display "1  10  100..."
+            // Linear axis scaling is suitable for our X-values, but not our erratic Y-values 
+            // We can set the scaling of the Y-axis to Logarithmic with a base of 20
+            // The Y-axis will now display "1, 20, 400, 8000...", which is ideal for accurate representation of this set of Y-values
             chart.AxisY.Scaling.Type = AxisScaleType.Logarithmic;
-            Assert.AreEqual(10.0, chart.AxisX.Scaling.LogBase);
+            chart.AxisY.Scaling.LogBase = 20.0;
 
             doc.Save(ArtifactsDir + "Charts.AxisScaling.docx");
             //ExEnd
@@ -310,10 +306,9 @@ namespace ApiExamples
             // Assign a Y-value for each date 
             chart.Series.Add("Series 1", dates, new[] { 3.0, 4.7, 5.9, 7.1, 8.9 });
 
-            // Set the bounds for the X-axis so that dates from before 1980, and from 1990 and onwards are outside of the bounds
-            // This will exclude the first and the last of our five values
-            // Note that while the excluded values do not appear in the viewport, they still exist in the graph,
-            // seeing as the data line in the line graph still tends towards them
+            // These particular bounds will cut off categories from before 1980 and from 1990 and onwards
+            // This narrows the amount of categories and values in the viewport from 5 to 3
+            // Note that the graph still contains the out-of-range data because we can see the line tend towards it
             chart.AxisX.Scaling.Minimum = new AxisBound(new DateTime(1980, 1, 1));
             chart.AxisX.Scaling.Maximum = new AxisBound(new DateTime(1990, 1, 1));
 
