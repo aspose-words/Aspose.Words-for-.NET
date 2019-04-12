27c7387822d7d1fd84b9727518bee1e461498bb0
romans91 <to.ru@Live.com>
2019-04-13 03:47:23 +1200

    BuiltInDocumentPropertiesContent examples
    Document properties from the "Content" section

--- ApiExamples/CSharp/ApiExamples/ExProperties.cs
+++ ApiExamples/CSharp/ApiExamples/ExProperties.cs
@@ -6,7 +6,10 @@
 //////////////////////////////////////////////////////////////////////////
 
 using System;
+using System.Drawing;
+using System.IO;
 using Aspose.Words;
+using Aspose.Words.Layout;
 using Aspose.Words.Properties;
 using NUnit.Framework;
 
@@ -67,7 +70,6 @@ namespace ApiExamples
                 DocumentProperty docProperty = doc.CustomDocumentProperties[i];
                 Console.WriteLine("{0}({1}) : {2}", docProperty.Name, docProperty.Type, docProperty.Value);
             }
-
             //ExEnd
         }
 
@@ -90,10 +92,7 @@ namespace ApiExamples
         {
             //ExStart
             //ExFor:BuiltInDocumentProperties.Author
-            //ExFor:BuiltInDocumentProperties.Bytes
             //ExFor:BuiltInDocumentProperties.Category
-            //ExFor:BuiltInDocumentProperties.Characters
-            //ExFor:BuiltInDocumentProperties.CharactersWithSpaces
             //ExFor:BuiltInDocumentProperties.Comments
             //ExFor:BuiltInDocumentProperties.Company
             //ExFor:BuiltInDocumentProperties.CreatedTime
@@ -101,28 +100,21 @@ namespace ApiExamples
             //ExFor:BuiltInDocumentProperties.LastPrinted
             //ExFor:BuiltInDocumentProperties.LastSavedBy
             //ExFor:BuiltInDocumentProperties.LastSavedTime
-            //ExFor:BuiltInDocumentProperties.Lines
             //ExFor:BuiltInDocumentProperties.Manager
             //ExFor:BuiltInDocumentProperties.NameOfApplication
-            //ExFor:BuiltInDocumentProperties.Pages
-            //ExFor:BuiltInDocumentProperties.Paragraphs
             //ExFor:BuiltInDocumentProperties.RevisionNumber
             //ExFor:BuiltInDocumentProperties.Subject
             //ExFor:BuiltInDocumentProperties.Template
             //ExFor:BuiltInDocumentProperties.Title
             //ExFor:BuiltInDocumentProperties.TotalEditingTime
             //ExFor:BuiltInDocumentProperties.Version
-            //ExFor:BuiltInDocumentProperties.Words
             //ExSummary:Retrieves information from the built-in document properties.
             String fileName = MyDir + "Properties.doc";
             Document doc = new Document(fileName);
 
             Console.WriteLine("Document name: {0}", fileName);
             Console.WriteLine("Document author: {0}", doc.BuiltInDocumentProperties.Author);
-            Console.WriteLine("Bytes: {0}", doc.BuiltInDocumentProperties.Bytes);
             Console.WriteLine("Category: {0}", doc.BuiltInDocumentProperties.Category);
-            Console.WriteLine("Characters: {0}", doc.BuiltInDocumentProperties.Characters);
-            Console.WriteLine("Characters with spaces: {0}", doc.BuiltInDocumentProperties.CharactersWithSpaces);
             Console.WriteLine("Comments: {0}", doc.BuiltInDocumentProperties.Comments);
             Console.WriteLine("Company: {0}", doc.BuiltInDocumentProperties.Company);
             Console.WriteLine("Create time: {0}", doc.BuiltInDocumentProperties.CreatedTime);
@@ -133,17 +125,137 @@ namespace ApiExamples
             Console.WriteLine("Lines: {0}", doc.BuiltInDocumentProperties.Lines);
             Console.WriteLine("Manager: {0}", doc.BuiltInDocumentProperties.Manager);
             Console.WriteLine("Name of application: {0}", doc.BuiltInDocumentProperties.NameOfApplication);
-            Console.WriteLine("Pages: {0}", doc.BuiltInDocumentProperties.Pages);
-            Console.WriteLine("Paragraphs: {0}", doc.BuiltInDocumentProperties.Paragraphs);
             Console.WriteLine("Revision number: {0}", doc.BuiltInDocumentProperties.RevisionNumber);
             Console.WriteLine("Subject: {0}", doc.BuiltInDocumentProperties.Subject);
             Console.WriteLine("Template: {0}", doc.BuiltInDocumentProperties.Template);
             Console.WriteLine("Title: {0}", doc.BuiltInDocumentProperties.Title);
             Console.WriteLine("Total editing time: {0}", doc.BuiltInDocumentProperties.TotalEditingTime);
             Console.WriteLine("Version: {0}", doc.BuiltInDocumentProperties.Version);
-            Console.WriteLine("Words: {0}", doc.BuiltInDocumentProperties.Words);
             //ExEnd
         }
+
+        //ExStart
+        //ExFor:BuiltInDocumentProperties.Bytes
+        //ExFor:BuiltInDocumentProperties.Characters
+        //ExFor:BuiltInDocumentProperties.CharactersWithSpaces
+        //ExFor:BuiltInDocumentProperties.ContentStatus
+        //ExFor:BuiltInDocumentProperties.ContentType
+        //ExFor:BuiltInDocumentProperties.Lines
+        //ExFor:BuiltInDocumentProperties.Pages
+        //ExFor:BuiltInDocumentProperties.Paragraphs
+        //ExFor:BuiltInDocumentProperties.Words
+        //ExSummary:Shows how to work with document properties from the "Content" category.
+        [Test] //ExSkip
+        public void BuiltInPropertiesContent()
+        {
+            // Create a new document and populate it with paragraphs and text
+            Document doc = new Document();
+            doc.RemoveAllChildren();
+            DocumentBuilder builder = new DocumentBuilder(doc);
+            builder.Writeln("Paragraph 1.");
+            builder.InsertBreak(BreakType.PageBreak);
+            builder.Write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+            builder.Write("Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.");
+            builder.Writeln("Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.");
+            builder.InsertBreak(BreakType.PageBreak);
+            builder.Writeln("Paragraph 3.");
+
+            // A document's built in properties can be viewed without opening the document,
+            // by right-clicking the file in Windows Explorer, navigating to Properties > Details
+            // The "Content" category will have the properties we are about to work with
+            // They are not updated automatically; their values need to be sourced from the document and assigned manually
+            BuiltInDocumentProperties properties = doc.BuiltInDocumentProperties;
+
+            // Page count: The number of pages is in a document's PageCount attribute, which is always up to date,
+            // and can be transferred to a built in property at any time
+            properties.Pages = doc.PageCount;
+            Assert.AreEqual(3, properties.Pages);
+
+            // Word count: The UpdateWordCount() automatically assigns the real time word/character counts to the relevant properties
+            doc.UpdateWordCount();
+            Assert.AreEqual(54, properties.Words);
+            Assert.AreEqual(305, properties.Characters);
+            Assert.AreEqual(356, properties.CharactersWithSpaces);
+
+            // Line count: Count the lines in a document and assign value to the property
+            LineCounter lineCounter = new LineCounter(doc);
+            properties.Lines = lineCounter.GetLineCount();
+            Assert.AreEqual(6, properties.Lines);
+
+            // Paragraph count: Assign the size of the array of all paragraphs to the Paragraphs property
+            properties.Paragraphs = doc.GetChildNodes(NodeType.Paragraph, true).Count;
+            Assert.AreEqual(4, properties.Paragraphs);
+
+            // Bytes: Get the real document file size from a stream and assign it to the property
+            using (MemoryStream stream = new MemoryStream())
+            {
+                doc.Save(stream, SaveFormat.Docx);
+                properties.Bytes = (int)stream.Length;
+                Assert.AreEqual(5756, properties.Bytes);
+            }
+            
+            // Template: If we change a document's template, the Template property will need a descriptive name for the template added manually
+            doc.AttachedTemplate = MyDir + "Document.BusinessBrochureTemplate.dot";
+            Assert.AreEqual("Normal.dot", properties.Template);          
+            properties.Template = "Document.BusinessBrochureTemplate.dot";
+
+            // Content status: This is a descriptive field that we can glance at without opening the document
+            properties.ContentStatus = "Draft";
+
+            // Content type: Upon saving, any value we assign to this field will be overwritten by the MIME type of the output save format
+            Assert.AreEqual("", properties.ContentType);
+
+            // If the document contains links that are up to date, we can set this to true
+            Assert.False(properties.LinksUpToDate);
+            
+            doc.Save(ArtifactsDir + "Properties.BuiltInPropertiesContent.docx");
+        }
+
+        /// <summary>
+        /// Util class that counts the lines in a document
+        /// Upon construction, traverses the document's layout entities tree, counting entities of the "Line" type that also contain real text
+        /// </summary>
+        private class LineCounter
+        {
+            public LineCounter(Document doc)
+            {
+                mLayoutEnumerator = new LayoutEnumerator(doc);
+
+                CountLines();
+            }
+
+            public int GetLineCount()
+            {
+                return mLineCount;
+            }
+
+            private void CountLines()
+            {
+                do
+                {
+                    if (mLayoutEnumerator.Type == LayoutEntityType.Line)
+                    {
+                        mScanningLineForRealText = true;
+                    }
+
+                    if (mLayoutEnumerator.MoveFirstChild())
+                    {
+                        if (mScanningLineForRealText && mLayoutEnumerator.Kind.StartsWith("TEXT"))
+                        {
+                            mLineCount++;
+                            mScanningLineForRealText = false;
+                        }
+                        CountLines();
+                        mLayoutEnumerator.MoveParent();
+                    }
+                } while (mLayoutEnumerator.MoveNext());
+            }
+
+            private readonly LayoutEnumerator mLayoutEnumerator;
+            private int mLineCount;
+            private bool mScanningLineForRealText;
+        }
+        //ExEnd
 
         [Test]
         public void CustomNamedAccess()
