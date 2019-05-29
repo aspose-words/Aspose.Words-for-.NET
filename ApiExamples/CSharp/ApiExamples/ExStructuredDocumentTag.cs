d63a433b10bedaacf56ecb8dbcb96543e2a4385d
romans91 <to.ru@Live.com>
2019-05-29 21:53:04 +1200

    Minor

--- ApiExamples/CSharp/ApiExamples/ExStructuredDocumentTag.cs
+++ ApiExamples/CSharp/ApiExamples/ExStructuredDocumentTag.cs
@@ -6,9 +6,7 @@
 //////////////////////////////////////////////////////////////////////////
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using Aspose.Words;
 using Aspose.Words.Markup;
 using NUnit.Framework;
