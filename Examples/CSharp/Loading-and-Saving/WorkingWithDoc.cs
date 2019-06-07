d367f1cbcc4ea92c0deb7f38b4c92ef4eae66a10
muzammil-khan <muzammil.khan@aspose.com>
2019-06-08 02:39:51 +0500

    Updates for Aspose.Words for .NET 19.6.

--- Examples/CSharp/Loading-and-Saving/WorkingWithDoc.cs
+++ Examples/CSharp/Loading-and-Saving/WorkingWithDoc.cs
@@ -15,6 +15,7 @@ namespace Aspose.Words.Examples.CSharp.Loading_Saving
             string dataDir = RunExamples.GetDataDir_LoadingAndSaving();
 
             EncryptDocumentWithPassword(dataDir);
+            AlwaysCompressMetafiles(dataDir);
         }
 
         public static void EncryptDocumentWithPassword(string dataDir)
@@ -27,6 +28,18 @@ namespace Aspose.Words.Examples.CSharp.Loading_Saving
             doc.Save(dataDir, docSaveOptions);
             //ExEnd:EncryptDocumentWithPassword
             Console.WriteLine("\nThe password of document is set using RC4 encryption method. \nFile saved at " + dataDir);
+        }
+
+        public static void AlwaysCompressMetafiles(string dataDir)
+        {
+            //ExStart:AlwaysCompressMetafiles
+            Document doc = new Document(dataDir + "Document.doc");
+            DocSaveOptions saveOptions = new DocSaveOptions();
+
+            saveOptions.AlwaysCompressMetafiles = false;
+            doc.Save("SmallMetafilesUncompressed.doc", saveOptions);
+            //ExEnd:AlwaysCompressMetafiles
+            Console.WriteLine("\nThe document is saved with AlwaysCompressMetafiles setting to false. \nFile saved at " + dataDir);
         }
     }
 }
