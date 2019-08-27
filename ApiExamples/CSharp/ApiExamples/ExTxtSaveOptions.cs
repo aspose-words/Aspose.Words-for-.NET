888b79d30985f975101dddb8f9706b34a24285bf
romans91 <to.ru@Live.com>
2019-08-27 14:01:11 +1200

    All examples

--- ApiExamples/CSharp/ApiExamples/ExTxtSaveOptions.cs
+++ ApiExamples/CSharp/ApiExamples/ExTxtSaveOptions.cs
@@ -71,15 +71,80 @@ namespace ApiExamples
             //ExFor:TxtListIndentation
             //ExFor:TxtListIndentation.Count
             //ExFor:TxtListIndentation.Character
-            //ExSummary:Shows how list levels are displayed when the document is converting to plain text format
+            //ExFor:TxtSaveOptions.ListIndentation
+            //ExSummary:Shows how list levels are displayed when the document is converting to plain text format.
             Document doc = new Document(MyDir + "TxtSaveOptions.TxtListIndentation.docx");
  
             TxtSaveOptions txtSaveOptions = new TxtSaveOptions();
             txtSaveOptions.ListIndentation.Count = 3;
             txtSaveOptions.ListIndentation.Character = ' ';
             txtSaveOptions.PreserveTableLayout = true;
- 
+
             doc.Save(ArtifactsDir + "TxtSaveOptions.TxtListIndentation.txt", txtSaveOptions);
+            //ExEnd
+        }
+
+        [Test]
+        public void ParagraphBreak()
+        {
+            //ExStart
+            //ExFor:TxtSaveOptions
+            //ExFor:TxtSaveOptions.SaveFormat
+            //ExFor:TxtSaveOptionsBase
+            //ExFor:TxtSaveOptionsBase.ParagraphBreak
+            //ExSummary:Shows how to save a .txt document with a custom paragraph break.
+            // Create a new document and add some paragraphs
+            Document doc = new Document();
+            DocumentBuilder builder = new DocumentBuilder(doc);
+            builder.Writeln("Paragraph 1.");
+            builder.Writeln("Paragraph 2.");
+            builder.Write("Paragraph 3.");
+
+            // When saved to plain text, the paragraphs we created can be separated by a custom string
+            TxtSaveOptions txtSaveOptions = new TxtSaveOptions {SaveFormat = SaveFormat.Text};
+            txtSaveOptions.ParagraphBreak = " End of paragraph.\n\n\t";
+
+            doc.Save(ArtifactsDir + "TxtSaveOptions.ParagraphBreak.txt", txtSaveOptions);
+            //ExEnd
+        }
+
+        [Test]
+        public void Encoding()
+        {
+            //ExStart
+            //ExFor:TxtSaveOptionsBase.Encoding
+            //ExSummary:Shows how to set encoding for a .txt output document.
+            // Create a new document and add some text from outside the ASCII character set
+            Document doc = new Document();
+            DocumentBuilder builder = new DocumentBuilder(doc);
+            builder.Writeln("À È Ì Ò Ù.");
+
+            // We can use a SaveOptions object to make sure the encoding we save the .txt document in supports our content
+            TxtSaveOptions txtSaveOptions = 
+                new TxtSaveOptions { SaveFormat = SaveFormat.Text, Encoding = System.Text.Encoding.UTF8 };
+
+            doc.Save(ArtifactsDir + "TxtSaveOptions.Encoding.txt", txtSaveOptions);
+            //ExEnd
+        }
+
+        [Test]
+        public void Appearance()
+        {
+            //ExStart
+            //ExFor:TxtSaveOptionsBase.PreserveTableLayout
+            //ExFor:TxtSaveOptionsBase.SimplifyListLabels
+            //ExSummary:Shows how to change the appearance of tables and lists during conversion to a txt document output.
+            // Open a document with a table
+            Document doc = new Document(MyDir + "Lists.PrintOutAllLists.doc");
+
+            // Due to the nature of text documents, table grids and text wrapping will be lost during conversion
+            // from a file type that supports tables
+            // We can preserve some of the table layout in the appearance of our content with the PreserveTableLayout flag
+            // The SimplifyListLabels flag will convert some list symbols
+            // into ASCII characters such as *, o, +, > etc, depending on list level
+            TxtSaveOptions txtSaveOptions = new TxtSaveOptions { SimplifyListLabels = true, PreserveTableLayout = true};
+
+            doc.Save(ArtifactsDir + "TxtSaveOptions.Appearance.txt", txtSaveOptions);
             //ExEnd
         }
     }
