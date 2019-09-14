15e891bcbf55911e6c70bbb4af3a35794db4a5c1
muzammil-khan <muzammil.khan@aspose.com>
2019-09-15 00:55:08 +0500

    Updates for Aspose.Words for .NET 19.9

--- Examples/CSharp/Loading-and-Saving/WorkingWithDoc.cs
+++ Examples/CSharp/Loading-and-Saving/WorkingWithDoc.cs
@@ -16,6 +16,7 @@ namespace Aspose.Words.Examples.CSharp.Loading_Saving
 
             EncryptDocumentWithPassword(dataDir);
             AlwaysCompressMetafiles(dataDir);
+            SavePictureBullet(dataDir);
         }
 
         public static void EncryptDocumentWithPassword(string dataDir)
@@ -37,9 +38,20 @@ namespace Aspose.Words.Examples.CSharp.Loading_Saving
             DocSaveOptions saveOptions = new DocSaveOptions();
 
             saveOptions.AlwaysCompressMetafiles = false;
-            doc.Save("SmallMetafilesUncompressed.doc", saveOptions);
+            doc.Save(dataDir + "SmallMetafilesUncompressed.doc", saveOptions);
             //ExEnd:AlwaysCompressMetafiles
             Console.WriteLine("\nThe document is saved with AlwaysCompressMetafiles setting to false. \nFile saved at " + dataDir);
+        }
+
+        public static void SavePictureBullet(string dataDir)
+        {
+            //ExStart:SavePictureBullet
+            Document doc = new Document(dataDir + "in.doc");
+            DocSaveOptions saveOptions = (DocSaveOptions)SaveOptions.CreateSaveOptions(SaveFormat.Doc);
+            saveOptions.SavePictureBullet = false;
+            doc.Save(dataDir + "out.doc", saveOptions);
+            //ExEnd:SavePictureBullet
+            Console.WriteLine("\nThe document is saved with SavePictureBullet setting to false. \nFile saved at " + dataDir);
         }
     }
 }
