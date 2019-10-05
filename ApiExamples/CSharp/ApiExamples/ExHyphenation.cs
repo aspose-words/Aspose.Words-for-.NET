fc1e0cb9f47bf02914a6a12225b716ed946c01a3
romans91 <to.ru@Live.com>
2019-10-05 23:32:23 +1300

    IHyphenationCallback example

--- ApiExamples/CSharp/ApiExamples/ExHyphenation.cs
+++ ApiExamples/CSharp/ApiExamples/ExHyphenation.cs
@@ -5,6 +5,8 @@
 // "as is", without warranty of any kind, either expressed or implied.
 //////////////////////////////////////////////////////////////////////////
 
+using System;
+using System.Collections.Generic;
 using System.IO;
 using Aspose.Words;
 using NUnit.Framework;
@@ -14,24 +16,53 @@ namespace ApiExamples
     [TestFixture]
     public class ExHyphenation : ApiExampleBase
     {
-        [Test]
+        //ExStart
+        //ExFor:Hyphenation
+        //ExFor:Hyphenation.Callback
+        //ExFor:Hyphenation.RegisterDictionary(String, Stream)
+        //ExFor:Hyphenation.RegisterDictionary(String, String)
+        //ExFor:IHyphenationCallback
+        //ExFor:IHyphenationCallback.RequestDictionary(System.String)
+        //ExSummary:Shows how to open and register a dictionary from a file.
+        [Test] //ExSKip
         public void RegisterDictionaryEx()
         {
-            //ExStart
-            //ExFor:Hyphenation
-            //ExFor:Hyphenation.RegisterDictionary(String, Stream)
-            //ExFor:Hyphenation.RegisterDictionary(String, String)
-            //ExSummary:Shows how to open and register a dictionary from a file.
             Document doc = new Document(MyDir + "Document.doc");
-
-            // Register by String
-            Hyphenation.RegisterDictionary("en-US", MyDir + "hyph_en_US.dic");
 
             // Register by stream
             Stream dictionaryStream = new FileStream(MyDir + "hyph_de_CH.dic", FileMode.Open);
             Hyphenation.RegisterDictionary("de-CH", dictionaryStream);
-            //ExEnd
+
+            // Register by string via callback
+            Hyphenation.Callback = new HyphenationPrinter();
+            Hyphenation.Callback.RequestDictionary("en-US");
         }
+
+        /// <summary>
+        /// Associates ISO language codes with dictionary files for their respective languages
+        /// </summary>
+        private class HyphenationPrinter : IHyphenationCallback
+        {
+            public HyphenationPrinter()
+            {
+                mDictionaryFilenames = new Dictionary<string, string>
+                {
+                    { "en-US", MyDir + "hyph_en_US.dic" },
+                    { "de-CH", MyDir + "hyph_de_CH.dic" }
+                };
+            }
+
+            public void RequestDictionary(string language)
+            {
+                if (mDictionaryFilenames.ContainsKey(language) && !Hyphenation.IsDictionaryRegistered(language))
+                {
+                    Hyphenation.RegisterDictionary(language, mDictionaryFilenames[language]);
+                }
+            }
+
+            private readonly Dictionary<string, string> mDictionaryFilenames;
+        }
+        //ExEnd
 
         [Test]
         public void IsDictionaryRegisteredEx()
