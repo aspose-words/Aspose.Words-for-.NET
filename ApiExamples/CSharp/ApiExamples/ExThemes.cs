9799572757d77d172f8cd107c4ce337354b3a76f
romans91 <to.ru@Live.com>
2019-11-16 21:54:31 +1300

    Combined examples

--- ApiExamples/CSharp/ApiExamples/ExThemes.cs
+++ ApiExamples/CSharp/ApiExamples/ExThemes.cs
@@ -7,9 +7,7 @@
 
 using System;
 using System.Drawing;
-using System.IO;
 using Aspose.Words;
-using Aspose.Words.Saving;
 using Aspose.Words.Themes;
 using NUnit.Framework;
 
@@ -19,9 +17,14 @@ namespace ApiExamples
     public class ExThemes : ApiExampleBase
     {
         [Test]
-        public void ThemeColors()
+        public void CustomColorsAndFonts()
         {
             //ExStart
+            //ExFor:Document.Theme
+            //ExFor:Theme
+            //ExFor:Theme.Colors
+            //ExFor:Theme.MajorFonts
+            //ExFor:Theme.MinorFonts
             //ExFor:Themes.ThemeColors
             //ExFor:Themes.ThemeColors.Accent1
             //ExFor:Themes.ThemeColors.Accent2
@@ -35,12 +38,29 @@ namespace ApiExamples
             //ExFor:Themes.ThemeColors.Hyperlink
             //ExFor:Themes.ThemeColors.Light1
             //ExFor:Themes.ThemeColors.Light2
-            //ExSummary:Shows how to set custom theme colors.
+            //ExFor:Themes.ThemeFonts
+            //ExFor:Themes.ThemeFonts.ComplexScript
+            //ExFor:Themes.ThemeFonts.EastAsian
+            //ExFor:Themes.ThemeFonts.Latin
+            //ExSummary:Shows how to set custom theme colors and fonts.
             Document doc = new Document(MyDir + "ThemeColors.docx");
 
-            // This collection of colors corresponds to the color palette from Microsoft Word which appears when changing shading or font color 
-            ThemeColors colors = doc.Theme.Colors;
+            // This object gives us access to the document theme, which is a source of default fonts and colors
+            Theme theme = doc.Theme;
 
+            // These fonts will be inherited by some styles like "Heading 1" and "Subtitle"
+            theme.MajorFonts.Latin = "Courier New";
+            theme.MinorFonts.Latin = "Agency FB";
+
+            Assert.AreEqual(String.Empty, theme.MajorFonts.ComplexScript);
+            Assert.AreEqual(String.Empty, theme.MajorFonts.EastAsian);
+            Assert.AreEqual(String.Empty, theme.MinorFonts.ComplexScript);
+            Assert.AreEqual(String.Empty, theme.MinorFonts.EastAsian);
+
+            // This collection of colors corresponds to the color palette from Microsoft Word which appears when changing shading or font color 
+            ThemeColors colors = theme.Colors;
+
+            // We will set the color of each color palette column going from left to right like this
             colors.Dark1 = Color.MidnightBlue;
             colors.Light1 = Color.PaleGreen;
             colors.Dark2 = Color.Indigo;
@@ -57,104 +77,23 @@ namespace ApiExamples
             colors.Hyperlink = Color.Black;
             colors.FollowedHyperlink = Color.Gray;
 
-            doc.Save(ArtifactsDir + "Themes.ThemeColors.docx");
+            doc.Save(ArtifactsDir + "Themes.CustomColorsAndFonts.docx");
             //ExEnd
-        }
+            doc = new Document(ArtifactsDir + "Themes.CustomColorsAndFonts.docx");
 
-        [Test]
-        public void DocumentThemeProperties()
-        {
-            //ExStart
-            //ExFor:Theme
-            //ExFor:Theme.Colors
-            //ExFor:Theme.MajorFonts
-            //ExFor:Theme.MinorFonts
-            //ExFor:Themes.ThemeFonts
-            //ExFor:Themes.ThemeFonts.ComplexScript
-            //ExFor:Themes.ThemeFonts.EastAsian
-            //ExFor:Themes.ThemeFonts.Latin
-            //ExSummary:Show how to change document theme options.
-            Document doc = new Document();
-            // Get document theme and do something useful
-            Theme theme = doc.Theme;
+            Assert.AreEqual(Color.OrangeRed.ToArgb(), doc.Theme.Colors.Accent1.ToArgb());
+            Assert.AreEqual(Color.MidnightBlue.ToArgb(), doc.Theme.Colors.Dark1.ToArgb());
+            Assert.AreEqual(Color.Gray.ToArgb(), doc.Theme.Colors.FollowedHyperlink.ToArgb());
+            Assert.AreEqual(Color.Black.ToArgb(), doc.Theme.Colors.Hyperlink.ToArgb());
+            Assert.AreEqual(Color.PaleGreen.ToArgb(), doc.Theme.Colors.Light1.ToArgb());
 
-            theme.Colors.Accent1 = Color.Black;
-            theme.Colors.Dark1 = Color.Blue;
-            theme.Colors.FollowedHyperlink = Color.White;
-            theme.Colors.Hyperlink = Color.WhiteSmoke;
-            theme.Colors.Light1 = Color.Empty; //There is default Color.Black
-
-            theme.MajorFonts.ComplexScript = "Arial";
-            theme.MajorFonts.EastAsian = String.Empty;
-            theme.MajorFonts.Latin = "Times New Roman";
-
-            theme.MinorFonts.ComplexScript = String.Empty;
-            theme.MinorFonts.EastAsian = "Times New Roman";
-            theme.MinorFonts.Latin = "Arial";
-            //ExEnd
-
-            MemoryStream dstStream = new MemoryStream();
-            doc.Save(dstStream, SaveFormat.Docx);
-
-            Assert.AreEqual(Color.Black.ToArgb(), doc.Theme.Colors.Accent1.ToArgb());
-            Assert.AreEqual(Color.Blue.ToArgb(), doc.Theme.Colors.Dark1.ToArgb());
-            Assert.AreEqual(Color.White.ToArgb(), doc.Theme.Colors.FollowedHyperlink.ToArgb());
-            Assert.AreEqual(Color.WhiteSmoke.ToArgb(), doc.Theme.Colors.Hyperlink.ToArgb());
-            Assert.AreEqual(Color.Black.ToArgb(), doc.Theme.Colors.Light1.ToArgb());
-
-            Assert.AreEqual("Arial", doc.Theme.MajorFonts.ComplexScript);
+            Assert.AreEqual(String.Empty, doc.Theme.MajorFonts.ComplexScript);
             Assert.AreEqual(String.Empty, doc.Theme.MajorFonts.EastAsian);
-            Assert.AreEqual("Times New Roman", doc.Theme.MajorFonts.Latin);
+            Assert.AreEqual("Courier New", doc.Theme.MajorFonts.Latin);
 
             Assert.AreEqual(String.Empty, doc.Theme.MinorFonts.ComplexScript);
-            Assert.AreEqual("Times New Roman", doc.Theme.MinorFonts.EastAsian);
-            Assert.AreEqual("Arial", doc.Theme.MinorFonts.Latin);
-        }
-
-        [Test]
-        public void DocTheme()
-        {
-            //ExStart
-            //ExFor:Document.Theme
-            //ExSummary:Shows what we can do with the Themes property of Document.
-            Document doc = new Document();
-
-            // When creating a blank document, Aspose Words creates a default theme object
-            Theme theme = doc.Theme;
-
-            // These color properties correspond to the 10 color columns that you see 
-            // in the "Theme colors" section in the color selector menu when changing font or shading color
-            // We can view and edit the leading color for each column, and the five different tints that
-            // make up the rest of the column will be derived automatically from each leading color
-            // Aspose Words sets the defaults to what they are in the Microsoft Word default theme
-            Assert.AreEqual(Color.FromArgb(255, 255, 255, 255), theme.Colors.Light1);
-            Assert.AreEqual(Color.FromArgb(255, 0, 0, 0), theme.Colors.Dark1);
-            Assert.AreEqual(Color.FromArgb(255, 238, 236, 225), theme.Colors.Light2);
-            Assert.AreEqual(Color.FromArgb(255, 31, 73, 125), theme.Colors.Dark2);
-            Assert.AreEqual(Color.FromArgb(255, 79, 129, 189), theme.Colors.Accent1);
-            Assert.AreEqual(Color.FromArgb(255, 192, 80, 77), theme.Colors.Accent2);
-            Assert.AreEqual(Color.FromArgb(255, 155, 187, 89), theme.Colors.Accent3);
-            Assert.AreEqual(Color.FromArgb(255, 128, 100, 162), theme.Colors.Accent4);
-            Assert.AreEqual(Color.FromArgb(255, 75, 172, 198), theme.Colors.Accent5);
-            Assert.AreEqual(Color.FromArgb(255, 247, 150, 70), theme.Colors.Accent6);
-
-            // Hyperlink colors
-            Assert.AreEqual(Color.FromArgb(255, 0, 0, 255), theme.Colors.Hyperlink);
-            Assert.AreEqual(Color.FromArgb(255, 128, 0, 128), theme.Colors.FollowedHyperlink);
-
-            // These appear at the very top of the font selector in the "Theme Fonts" section
-            Assert.AreEqual("Cambria", theme.MajorFonts.Latin);
-            Assert.AreEqual("Calibri", theme.MinorFonts.Latin);
-
-            // Change some values to make a custom theme
-            theme.MinorFonts.Latin = "Bodoni MT";
-            theme.MajorFonts.Latin = "Tahoma";
-            theme.Colors.Accent1 = Color.Cyan;
-            theme.Colors.Accent2 = Color.Yellow;
-
-            // Save the document to use our theme
-            doc.Save(ArtifactsDir + "Document.Theme.docx");
-            //ExEnd
+            Assert.AreEqual(String.Empty, doc.Theme.MinorFonts.EastAsian);
+            Assert.AreEqual("Agency FB", doc.Theme.MinorFonts.Latin);
         }
     }
 }
