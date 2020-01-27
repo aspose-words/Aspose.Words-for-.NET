a839e7d07b30df4ea162c43f61ab7cc13a0ad70b
romans91 <to.ru@Live.com>
2020-01-27 21:53:05 +1300

    Minor

--- ApiExamples/CSharp/ApiExamples/ExTable.cs
+++ ApiExamples/CSharp/ApiExamples/ExTable.cs
@@ -1218,7 +1218,7 @@ namespace ApiExamples
             //ExFor:Table.AbsoluteHorizontalDistance
             //ExFor:Table.AbsoluteVerticalDistance
             //ExSummary:Shows how get/set properties for floating tables.
-            Document doc = new Document(MyDir + "Table.docx");
+            Document doc = new Document(MyDir + "Tables.docx");
 
             Table table = (Table) doc.GetChild(NodeType.Table, 0, true);
             table.AbsoluteHorizontalDistance = 10;
