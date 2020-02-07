8e76ee2d2ece9aa533b8d5503131afdf0e22bab1
Muzammil Khan <muzammil.khan@aspose.com>
2020-02-07 23:18:26 +0500

    Code Examples for Aspose.Words for .NET 20.2

--- Examples/CSharp/RunExamples.cs
+++ Examples/CSharp/RunExamples.cs
@@ -77,7 +77,7 @@ namespace Aspose.Words.Examples.CSharp
             // CheckFormat.Run();
             // SplitIntoHtmlPages.Run();
             // LoadTxt.Run();
-            // Load_Options.Run();
+             Load_Options.Run();
             // PageSplitter.Run();
             // ImageToPdf.Run();
             // SpecifySaveOption.Run();
@@ -96,7 +96,7 @@ namespace Aspose.Words.Examples.CSharp
             // SaveOptionsHtmlFixed.Run();
             // SaveDocWithHtmlSaveOptions.Run();
             // WorkingWithVbaMacros.Run();
-            WorkingWithPDF.Run();
+            
 
             //// =====================================================
             //// =====================================================
---
+++ Examples/ExamplesCore/Program.cs
@@ -0,0 +1,83 @@
+﻿using Aspose.Words;
+using Aspose.Words.Saving;
+using System;
+using System.IO;
+
+namespace ExamplesCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The path to the documents directory.
+            string dataDir = GetDataDir_LoadingAndSaving();
+
+            LoadPDF(dataDir);
+            LoadAndSaveEncryptedPDF(dataDir);
+            LoadPageRangeOfPDF(dataDir);
+        }
+
+        public static void LoadPDF(string dataDir)
+        {
+            //ExStart:LoadPDF
+            Document doc = new Document(dataDir + "Document.pdf");
+
+            dataDir = dataDir + "Document_out.pdf";
+            doc.Save(dataDir);
+            //ExEnd:LoadPDF
+            Console.WriteLine("\nDocument saved.\nFile saved at " + dataDir);
+        }
+
+        public static void LoadAndSaveEncryptedPDF(string dataDir)
+        {
+            // ExStart:LoadAndSaveEncryptedPDF  
+            PdfLoadOptions pdfLoad = new PdfLoadOptions();
+            pdfLoad.Password = "password";
+
+            Document doc = new Document(dataDir + @"encrypted.pdf", pdfLoad);
+            doc.Save(dataDir + "out.pdf");
+            // ExEnd:LoadAndSaveEncryptedPDF 
+            Console.WriteLine("\nLoad and save encrypted document successfully.\nFile saved at " + dataDir);
+        }
+
+        public static void LoadPageRangeOfPDF(string dataDir)
+        {
+            // ExStart:LoadPageRangeOfPDF  
+            PdfLoadOptions pdfLoad = new PdfLoadOptions();
+            pdfLoad.PageIndex = 0;
+            pdfLoad.PageCount = 2;
+
+            Document doc = new Document(dataDir + @"Document1.pdf", pdfLoad);
+            doc.Save(dataDir + "out.pdf");
+            // ExEnd:LoadPageRangeOfPDF 
+            Console.WriteLine("\nLoad and save PDF with specific Page Range successfully.\nFile saved at " + dataDir);
+        }
+
+        public static String GetDataDir_LoadingAndSaving()
+        {
+            return Path.GetFullPath(GetDataDir_Data() + "Loading-and-Saving/");
+        }
+        private static string GetDataDir_Data()
+        {
+            var parent = Directory.GetParent(Directory.GetCurrentDirectory()).Parent;
+            string startDirectory = null;
+            if (parent != null)
+            {
+                var directoryInfo = parent.Parent;
+                if (directoryInfo != null)
+                {
+                    var directoryPInfo = directoryInfo.Parent;
+                    startDirectory = directoryPInfo.FullName;
+                }
+            }
+            else
+            {
+                startDirectory = parent.FullName;
+            }
+
+            return Path.Combine(startDirectory, "Data\\");
+        }
+
+
+    }
+}
