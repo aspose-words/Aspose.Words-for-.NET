9af2f7f60ffed1001d2544b71af4d9c55b0553b4
muzammil-khan <muzammil.khan@aspose.com>
2020-02-07 21:26:14 +0500

    Create WorkingWithPDF.cs

---
+++ Examples/CSharp/Loading-and-Saving/WorkingWithPDF.cs
@@ -0,0 +1,31 @@
+﻿using Aspose.Words.Saving;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aspose.Words.Examples.CSharp.Loading_and_Saving
+{
+    class WorkingWithPDF
+    {
+        public static void Run()
+        {
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_LoadingAndSaving();
+
+            LoadPDF(dataDir);
+        }
+
+        public static void LoadPDF(string dataDir)
+        {
+            //ExStart:LoadPDF
+            Document doc = new Document(dataDir + "Document.pdf");
+            
+            dataDir = dataDir + "Document_out.pdf";
+            doc.Save(dataDir);
+            //ExEnd:LoadPDF
+            Console.WriteLine("\nDocument saved.\nFile saved at " + dataDir);
+        }
+    }
+}
