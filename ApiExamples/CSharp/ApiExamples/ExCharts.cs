ee8962a1a6b1e168d0351bc44a7cd196d7eab35b
romans91 <to.ru@Live.com>
2020-02-12 19:18:32 +1300

    DigitalSignatureCollection examples unit tests

--- ApiExamples/CSharp/ApiExamples/ExCharts.cs
+++ ApiExamples/CSharp/ApiExamples/ExCharts.cs
@@ -6,7 +6,6 @@
 //////////////////////////////////////////////////////////////////////////
 
 using System;
-using System.IO;
 using System.Collections.Generic;
 using Aspose.Words;
 using Aspose.Words.Drawing;
