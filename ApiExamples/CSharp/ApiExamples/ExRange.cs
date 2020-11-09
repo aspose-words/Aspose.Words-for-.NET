584e023f3578ffc1d8ea0a7b0890c7d7eaabb6b2
romans91 <to.ru@Live.com>
2020-11-09 23:19:10 +1300

    Created dedicated example for FindReplaceOptions.Direction

--- ApiExamples/CSharp/ApiExamples/ExRange.cs
+++ ApiExamples/CSharp/ApiExamples/ExRange.cs
@@ -332,7 +332,6 @@ namespace ApiExamples
 
         //ExStart
         //ExFor:FindReplaceOptions.ApplyFont
-        //ExFor:FindReplaceOptions.Direction
         //ExFor:FindReplaceOptions.ReplacingCallback
         //ExFor:ReplacingArgs.GroupIndex
         //ExFor:ReplacingArgs.GroupName
@@ -355,9 +354,6 @@ namespace ApiExamples
             // Set the "HighlightColor" property to a background color that we want to apply to the resulting text of the operation.
             options.ApplyFont.HighlightColor = Color.LightGray;
             
-            // Use the "Direction" property to set the direction of the find-and-replace text search.
-            options.Direction = FindReplaceDirection.Backward;
-
             NumberHexer numberHexer = new NumberHexer();
             options.ReplacingCallback = numberHexer;
 
@@ -582,7 +578,6 @@ namespace ApiExamples
         //ExFor:IReplacingCallback.Replacing
         //ExFor:ReplacingArgs
         //ExFor:ReplacingArgs.MatchNode
-        //ExFor:FindReplaceDirection
         //ExSummary:Shows how to insert the contents of an entire document as replacement of a match in a find-and-replace operation.
         [Test] //ExSkip
         public void InsertDocumentAtReplace()
@@ -591,7 +586,6 @@ namespace ApiExamples
 
             // We can use a "FindReplaceOptions" object to modify the find-and-replace process.
             FindReplaceOptions options = new FindReplaceOptions();
-            options.Direction = FindReplaceDirection.Backward;
             options.ReplacingCallback = new InsertDocumentAtReplaceHandler();
 
             mainDoc.Range.Replace(new Regex("\\[MY_DOCUMENT\\]"), "", options);
@@ -660,5 +654,69 @@ namespace ApiExamples
                             "2) At a MERGEFIELD:\r\u0013 MERGEFIELD  Document_1  \\* MERGEFORMAT \u0014«Document_1»\u0015\r" +
                             "3) At a bookmark:", doc.FirstSection.Body.GetText().Trim());
         }
+
+        //ExStart
+        //ExFor:FindReplaceOptions.Direction
+        //ExFor:FindReplaceDirection
+        //ExSummary:Shows how to determine which direction a find-and-replace operation traverses the document in.
+        [TestCase(FindReplaceDirection.Backward)] //ExSkip
+        [TestCase(FindReplaceDirection.Forward)] //ExSkip
+        public void Direction(FindReplaceDirection findReplaceDirection)
+        {
+            Document doc = new Document();
+            DocumentBuilder builder = new DocumentBuilder(doc);
+
+            // Insert three runs which we can search for using a regex pattern.
+            // Place one of those runs inside a text box.
+            builder.Writeln("Match 1.");
+            builder.Writeln("Match 2.");
+            builder.Writeln("Match 3.");
+            builder.Writeln("Match 4.");
+
+            // We can use a "FindReplaceOptions" object to modify the find-and-replace process.
+            FindReplaceOptions options = new FindReplaceOptions();
+
+            // Assign a custom callback to the "ReplacingCallback" property.
+            TextReplacementRecorder callback = new TextReplacementRecorder();
+            options.ReplacingCallback = callback;
+
+            // Set the "Direction" property to "FindReplaceDirection.Backward" to get the find-and-replace
+            // operation to start from the end of the range, and traverse back to the beginning.
+            // Set the "Direction" property to "FindReplaceDirection.Backward" to get the find-and-replace
+            // operation to start from the beginning of the range, and traverse to the end.
+            options.Direction = findReplaceDirection;
+
+            doc.Range.Replace(new Regex(@"Match \d*"), "Replacement", options);
+
+            Assert.AreEqual("Replacement.\r" +
+                            "Replacement.\r" +
+                            "Replacement.\r" +
+                            "Replacement.", doc.GetText().Trim());
+
+            switch (findReplaceDirection)
+            {
+                case FindReplaceDirection.Forward:
+                    Assert.AreEqual(new[] {"Match 1", "Match 2", "Match 3", "Match 4"}, callback.Matches);
+                    break;
+                case FindReplaceDirection.Backward:
+                    Assert.AreEqual(new[] { "Match 4", "Match 3", "Match 2", "Match 1" }, callback.Matches);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Records all matches that occur during a find-and-replace operation in the order that they take place.
+        /// </summary>
+        private class TextReplacementRecorder : IReplacingCallback
+        {
+            ReplaceAction IReplacingCallback.Replacing(ReplacingArgs e)
+            {
+                Matches.Add(e.Match.Value);
+                return ReplaceAction.Replace;
+            }
+
+            public List<string> Matches { get; } = new List<string>();
+        }
+        //ExEnd
     }
 }
