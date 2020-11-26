fdb6e7ec7b891d88c4dbec4e0749fa32c5a955a3
vderyushev <vyacheslav.deryushev@aspose.com>
2020-11-26 13:48:26 +0700

    Removed font count assert

--- ApiExamples/CSharp/ApiExamples/ExFontSettings.cs
+++ ApiExamples/CSharp/ApiExamples/ExFontSettings.cs
@@ -417,7 +417,6 @@ namespace ApiExamples
             FontSourceBase[] originalFontSources = FontSettings.DefaultInstance.GetFontsSources();
 
             Assert.AreEqual(1, originalFontSources.Length);
-            Assert.AreEqual(479, originalFontSources[0].GetAvailableFonts().Count);
             Assert.True(originalFontSources[0].GetAvailableFonts().Any(f => f.FullFontName == "Arial"));
 
             // The default font sources are missing the two fonts that we are using in this document.
@@ -478,7 +477,6 @@ namespace ApiExamples
             FontSourceBase[] originalFontSources = FontSettings.DefaultInstance.GetFontsSources();
 
             Assert.AreEqual(1, originalFontSources.Length);
-            Assert.AreEqual(479, originalFontSources[0].GetAvailableFonts().Count);
             Assert.True(originalFontSources[0].GetAvailableFonts().Any(f => f.FullFontName == "Arial"));
 
             // The default font sources are missing the two fonts that we are using in this document.
