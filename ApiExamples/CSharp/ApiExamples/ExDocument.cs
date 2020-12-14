7000d8384c89152a4e3e54174dba4d66b87d073d
romans91 <to.ru@Live.com>
2020-12-14 20:07:53 +1300

    Moved UpdateTableLayout example to ExDocument.cs

--- ApiExamples/CSharp/ApiExamples/ExDocument.cs
+++ ApiExamples/CSharp/ApiExamples/ExDocument.cs
@@ -988,6 +988,40 @@ namespace ApiExamples
         }
 
         [Test]
+        public void UpdateTableLayout()
+        {
+            //ExStart
+            //ExFor:Document.UpdateTableLayout
+            //ExSummary:Shows how to preserve a table's layout when saving to .txt.
+            Document doc = new Document();
+            DocumentBuilder builder = new DocumentBuilder(doc);
+
+            Table table = builder.StartTable();
+            builder.InsertCell();
+            builder.Write("Cell 1");
+            builder.InsertCell();
+            builder.Write("Cell 2");
+            builder.InsertCell();
+            builder.Write("Cell 3");
+            builder.EndTable();
+
+            // Use a TxtSaveOptions object to preserve the table's layout when converting the document to plaintext.
+            TxtSaveOptions options = new TxtSaveOptions();
+            options.PreserveTableLayout = true;
+
+            // Previewing the appearance of the document in .txt form shows that the table will not be represented accurately.
+            Assert.AreEqual(0.0d, table.FirstRow.Cells[0].CellFormat.Width);
+            Assert.AreEqual("CCC\r\neee\r\nlll\r\nlll\r\n   \r\n123\r\n\r\n", doc.ToString(options));
+
+            // We can call UpdateTableLayout() to fix some of these issues.
+            doc.UpdateTableLayout();
+
+            Assert.AreEqual("Cell 1             Cell 2             Cell 3\r\n\r\n", doc.ToString(options));
+            Assert.AreEqual(155.0d, table.FirstRow.Cells[0].CellFormat.Width, 2f);
+            //ExEnd
+        }
+
+        [Test]
         public void GetOriginalFileInfo()
         {
             //ExStart
