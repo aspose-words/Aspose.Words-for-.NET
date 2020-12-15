26bf6ee069c9567082995094af34359e311f75d6
romans91 <to.ru@Live.com>
2020-12-15 21:18:33 +1300

    ExVariableCollection comments

--- ApiExamples/CSharp/ApiExamples/ExVariableCollection.cs
+++ ApiExamples/CSharp/ApiExamples/ExVariableCollection.cs
@@ -35,14 +35,14 @@ namespace ApiExamples
             Document doc = new Document();
             VariableCollection variables = doc.Variables;
 
-            // Documents have a variable collection to which name/value pairs can be added
+            // Every document has a collection of key/value pair variables, which we can add items to.
             variables.Add("Home address", "123 Main St.");
             variables.Add("City", "London");
             variables.Add("Bedrooms", "3");
 
             Assert.AreEqual(3, variables.Count);
 
-            // Variables can be referenced and have their values presented in the document by DOCVARIABLE fields
+            // We can display the values of variables in the document body using DOCVARIABLE fields.
             DocumentBuilder builder = new DocumentBuilder(doc);
             FieldDocVariable field = (FieldDocVariable)builder.InsertField(FieldType.FieldDocVariable, true);
             field.VariableName = "Home address";
@@ -50,37 +50,42 @@ namespace ApiExamples
 
             Assert.AreEqual("123 Main St.", field.Result);
 
-            // Assigning values to existing keys will update them
+            // Assigning values to existing keys will update them.
             variables.Add("Home address", "456 Queen St.");
 
-            // DOCVARIABLE fields also need to be updated in order to show an accurate up to date value
+            // We will then have to update DOCVARIABLE fields to ensure they display an up-to-date value.
+            Assert.AreEqual("123 Main St.", field.Result);
+
             field.Update();
 
             Assert.AreEqual("456 Queen St.", field.Result);
 
-            // The existence of variables can be looked up either by name or value like this
+            // Verify that document variables with a certain name or value exist.
             Assert.True(variables.Contains("City"));
             Assert.True(variables.Any(v => v.Value == "London"));
 
-            // Variables are automatically sorted in alphabetical order
+            // The collection of variables automatically sorts variables alphabetically by name.
             Assert.AreEqual(0, variables.IndexOfKey("Bedrooms"));
             Assert.AreEqual(1, variables.IndexOfKey("City"));
             Assert.AreEqual(2, variables.IndexOfKey("Home address"));
 
-            // Enumerate over the collection of variables
+            // Enumerate over the collection of variables.
             using (IEnumerator<KeyValuePair<string, string>> enumerator = doc.Variables.GetEnumerator())
                 while (enumerator.MoveNext())
                     Console.WriteLine($"Name: {enumerator.Current.Key}, Value: {enumerator.Current.Value}");
 
-            // Variables can be removed either by name or index, or the entire collection can be cleared at once
+            // Below are three ways of removing document variables from a collection.
+            // 1 -  By name:
             variables.Remove("City");
 
             Assert.False(variables.Contains("City"));
 
+            // 2 -  By index:
             variables.RemoveAt(1);
 
             Assert.False(variables.Contains("Home address"));
 
+            // 3 -  Clear the whole collection at once:
             variables.Clear();
 
             Assert.That(variables, Is.Empty);
