01e31376a4e2464fee81e70ffe5f3228b3fb697b
romans91 <to.ru@live.com>
2021-01-11 19:54:58 +1300

    Refactored ExportContentToImages.2013.sln

--- Plugins/MVC/Aspose.MVC.ExportContentToImages/ExportContentToImages/App_Start/RouteConfig.cs
+++ Plugins/MVC/Aspose.MVC.ExportContentToImages/ExportContentToImages/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace ExportContentToImages
@@ -12,7 +8,6 @@ namespace ExportContentToImages
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
