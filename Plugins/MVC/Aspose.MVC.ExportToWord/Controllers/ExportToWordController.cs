1f92e62801925bfc6e0ae9c6985ab39858191e02
romans91 <to.ru@live.com>
2021-01-16 23:12:31 +1300

    Cosmetic

--- Plugins/MVC/Aspose.MVC.ExportToWord/Controllers/ExportToWordController.cs
+++ Plugins/MVC/Aspose.MVC.ExportToWord/Controllers/ExportToWordController.cs
@@ -1,55 +1,44 @@
 ﻿using Aspose.Words;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
 using System.Text;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Aspose.MVC.ExportToWord.Controllers
 {
     public class ExportToWordController : Controller
     {
-        //
         // GET: /ExportToWord/
-
         public void Index()
         {
-            string baseURL = Request.Url.Authority;
+            ApplyLicense();
 
-            if (Request.ServerVariables["HTTPS"] == "on")
-            {
-                baseURL = "https://" + baseURL;
-            }
-            else
-            {
-                baseURL = "http://" + baseURL;
-            }
+            string baseUrl = Request.Url.Authority;
+            baseUrl = Request.ServerVariables["HTTPS"] == "on" ? "https://" + baseUrl : "http://" + baseUrl;
+            
+            string refUrl = Request.UrlReferrer.AbsoluteUri;
+            string html = new WebClient().DownloadString(refUrl);
 
-            // Check for license and apply if exists
+            // To make the relative image paths work, the head section must include the base URL.
+            html = html.Replace("</head>", string.Format("<base href='{0}'></base></head>", baseUrl));
+
+            MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(html));
+            Document doc = new Document(stream);
+            string fileName = Guid.NewGuid() + ".doc";
+            doc.Save(System.Web.HttpContext.Current.Response, fileName, ContentDisposition.Inline, null);
+
+            System.Web.HttpContext.Current.Response.End();
+        }
+
+        private void ApplyLicense()
+        {
             string licenseFile = Server.MapPath("~/App_Data/Aspose.Words.lic");
             if (System.IO.File.Exists(licenseFile))
             {
                 License license = new License();
                 license.SetLicense(licenseFile);
             }
-
-            string refURL = Request.UrlReferrer.AbsoluteUri;
-
-            string html = new WebClient().DownloadString(refURL);
-
-            // To make the relative image paths work, base URL must be included in head section
-            html = html.Replace("</head>", string.Format("<base href='{0}'></base></head>", baseURL));
-
-            MemoryStream stream = new MemoryStream(Encoding.UTF8.GetBytes(html));
-            Document doc = new Document(stream);
-            string fileName = System.Guid.NewGuid().ToString() + ".doc";
-            doc.Save(System.Web.HttpContext.Current.Response, fileName, ContentDisposition.Inline, null);
-
-            System.Web.HttpContext.Current.Response.End();
         }
-
     }
 }
