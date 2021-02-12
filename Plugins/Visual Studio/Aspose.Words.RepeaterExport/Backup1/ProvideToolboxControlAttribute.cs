403ff6091fdcc5ade7b2f14c2110d7aa46fcb3ae
romans91 <to.ru@live.com>
2021-02-12 23:35:15 +1300

    Updated plugins for "Visual Studio" project "RepeaterExport"

--- Plugins/Visual Studio/Aspose.Words.RepeaterExport/Backup1/ProvideToolboxControlAttribute.cs
+++ Plugins/Visual Studio/Aspose.Words.RepeaterExport/Backup1/ProvideToolboxControlAttribute.cs
@@ -10,6 +10,9 @@
 //***************************************************************************
 
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Globalization;
 using Microsoft.VisualStudio.Shell;
 
