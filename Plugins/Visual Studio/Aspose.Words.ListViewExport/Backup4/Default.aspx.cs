26b9e2ecf0dbc169aa0d59a16f620b48a3322998
romans91 <to.ru@live.com>
2021-02-12 23:49:45 +1300

    Updated plugins for "Visual Studio" project "ListViewExport"

---
+++ Plugins/Visual Studio/Aspose.Words.ListViewExport/Backup4/Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+namespace Aspose.Words.ListViewExport.Website
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                BindListView();
+            }
+        }
+
+        private void BindListView()
+        {
+            DataTable dataTable = new DataTable("Products");
+            dataTable.Columns.Add("Product ID", typeof(Int32));
+            dataTable.Columns.Add("Product Name", typeof(string));
+            dataTable.Columns.Add("Units In Stock", typeof(Int32));
+
+
+            for (int index = 1; index <= 50; index++)
+            {
+                DataRow dr = dataTable.NewRow();
+                dr[0] = index;
+                dr[1] = "Product - " + dr[0];
+                dr[2] = index + 5;
+                dataTable.Rows.Add(dr);
+            }
+
+
+            ExportListViewToWord1.ExportDataSource = dataTable;
+            ExportListViewToWord1.DataSource = dataTable;
+            ExportListViewToWord1.DataBind();
+        }
+
+        protected void ExportListViewToWord1_PagePropertiesChanging(object sender, PagePropertiesChangingEventArgs e)
+        {
+            DataPager objDataPager1 = (DataPager)ExportListViewToWord1.FindControl("DataPager1");
+            if (objDataPager1 != null)
+            {
+                objDataPager1.SetPageProperties(e.StartRowIndex, e.MaximumRows, false);
+                BindListView();
+            }
+        }
+
+
+        public override void VerifyRenderingInServerForm(Control control)
+        { }
+
+    }
+}
