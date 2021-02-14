17f9a81375dc29d661c1e480faad1197f609d93d
romans91 <to.ru@live.com>
2021-02-14 23:52:51 +1300

    Combined two "Aspose.Words Vs NPOI HWPF and XWPF" versions into one solution

---
+++ Plugins/NPOI/Aspose.Words Vs NPOI HWPF and XWPF/Create Bullet/NPOI HWPF and XWPF/Program.cs
@@ -0,0 +1,109 @@
+﻿using System.IO;
+using NPOI.XWPF.UserModel;
+
+namespace NPOI_HWPF_and_XWPF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XWPFDocument doc = new XWPFDocument();
+
+            // Single level bullet.
+            XWPFNumbering numbering = doc.CreateNumbering();
+
+            string abstractNumId = numbering.AddAbstractNum();
+            string numId = numbering.AddNum(abstractNumId);
+
+            XWPFParagraph p0 = doc.CreateParagraph();
+            XWPFRun r0 = p0.CreateRun();
+            r0.SetText("simple bullet");
+            r0.IsBold = true;
+            r0.FontFamily = "Courier";
+            r0.FontSize = 12;
+
+            XWPFParagraph p1 = doc.CreateParagraph();
+            XWPFRun r1 = p1.CreateRun();
+            r1.SetText("first, create paragraph and run, set text");
+            p1.SetNumID(numId);
+
+            XWPFParagraph p2 = doc.CreateParagraph();
+            XWPFRun r2 = p2.CreateRun();
+            r2.SetText("second, call XWPFDocument.CreateNumbering() to create numbering");
+            p2.SetNumID(numId);
+
+            XWPFParagraph p3 = doc.CreateParagraph();
+            XWPFRun r3 = p3.CreateRun();
+            r3.SetText("third, add AbstractNum[numbering.AddAbstractNum()] and Num(numbering.AddNum(abstractNumId))");
+            p3.SetNumID(numId);
+
+            XWPFParagraph p4 = doc.CreateParagraph();
+            XWPFRun r4 = p4.CreateRun();
+            r4.SetText("next, call XWPFParagraph.SetNumID(numId) to set paragraph property, CT_P.pPr.numPr");
+            p4.SetNumID(numId);
+
+            // Multi level bullet.
+            abstractNumId = numbering.AddAbstractNum();
+            numId = numbering.AddNum(abstractNumId);
+            doc.CreateParagraph();
+            doc.CreateParagraph();
+
+            p1 = doc.CreateParagraph();
+            r1 = p1.CreateRun();
+            r1.SetText("multi level bullet");
+            r1.IsBold = true;
+            r1.FontFamily = "Courier";
+            r1.FontSize = 12;
+
+            p1 = doc.CreateParagraph();
+            r1 = p1.CreateRun();
+            r1.SetText("first");
+            p1.SetNumID(numId, "0");
+            p1 = doc.CreateParagraph();
+            r1 = p1.CreateRun();
+            r1.SetText("first-first");
+            p1.SetNumID(numId, "1");
+            p1 = doc.CreateParagraph();
+            r1 = p1.CreateRun();
+            r1.SetText("first-second");
+            p1.SetNumID(numId, "1");
+            p1 = doc.CreateParagraph();
+            r1 = p1.CreateRun();
+            r1.SetText("first-third");
+            p1.SetNumID(numId, "1");
+            p1 = doc.CreateParagraph();
+            r1 = p1.CreateRun();
+            r1.SetText("second");
+            p1.SetNumID(numId, "0");
+            p1 = doc.CreateParagraph();
+            r1 = p1.CreateRun();
+            r1.SetText("second-first");
+            p1.SetNumID(numId, "1");
+            p1 = doc.CreateParagraph();
+            r1 = p1.CreateRun();
+            r1.SetText("second-second");
+            p1.SetNumID(numId, "1");
+            p1 = doc.CreateParagraph();
+            r1 = p1.CreateRun();
+            r1.SetText("second-third");
+            p1.SetNumID(numId, "1");
+            p1 = doc.CreateParagraph();
+            r1 = p1.CreateRun();
+            r1.SetText("second-third-first");
+            p1.SetNumID(numId, "2");
+            p1 = doc.CreateParagraph();
+            r1 = p1.CreateRun();
+            r1.SetText("second-third-second");
+            p1.SetNumID(numId, "2");
+            p1 = doc.CreateParagraph();
+            r1 = p1.CreateRun();
+            r1.SetText("third");
+            p1.SetNumID(numId, "0");
+
+            using (FileStream sw = new FileStream("CreateBulletNPOI.docx", FileMode.Create))
+            {
+                doc.Write(sw);
+            }
+        }
+    }
+}
---
+++ Plugins/NPOI/Aspose.Words Vs NPOI HWPF and XWPF/Create Table/Aspose.Words/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+
+namespace Aspose.Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Check for license, and apply if it exists.
+            string licenseFile = AppDomain.CurrentDomain.BaseDirectory + "Aspose.Words.lic";
+            if (File.Exists(licenseFile))
+            {
+                // Apply Aspose.Words API License.
+                Aspose.Words.License license = new Aspose.Words.License();
+                // Place license file in Bin/Debug/ Folder.
+                license.SetLicense("Aspose.Words.lic");
+            }
+
+            Document doc = new Document();
+            DocumentBuilder builder = new DocumentBuilder(doc);
+
+            builder.ParagraphFormat.Borders.Top.LineStyle = LineStyle.Thick;
+            builder.ParagraphFormat.Shading.BackgroundPatternColor = System.Drawing.ColorTranslator.FromHtml("#EEEEEE");
+            builder.ParagraphFormat.Shading.Texture = TextureIndex.TextureDarkDiagonalUp;
+            builder.Writeln("Title1");
+
+            builder.ParagraphFormat.ClearFormatting();
+            builder.InsertBreak(BreakType.ParagraphBreak);
+
+            // Call this method to start building the table.
+            builder.StartTable();
+            builder.InsertCell();
+
+            builder.CellFormat.Shading.BackgroundPatternColor = System.Drawing.ColorTranslator.FromHtml("#FF0000");
+            builder.Font.Position = 100;
+            builder.Font.Name = "Courier";
+            builder.Font.Bold = true;
+            builder.Font.Underline = Underline.DotDotDash;
+            builder.Write("The quick brown fox");
+            builder.InsertCell();
+
+            builder.Font.ClearFormatting();
+            builder.CellFormat.ClearFormatting();
+
+            builder.InsertCell();
+            builder.EndRow();
+
+            builder.InsertCell();
+            builder.InsertCell();
+            builder.Write("EXAMPLE OF TABLE");
+            builder.InsertCell();
+            builder.EndRow();
+
+            builder.InsertCell();
+            builder.InsertCell();
+            builder.InsertCell();
+            builder.Write("only text");
+            builder.EndRow();
+
+            // Signal that we have finished building the table.
+            builder.EndTable();
+
+            doc.Save("SimpleTableAspose.docx");
+        }
+    }
+}
