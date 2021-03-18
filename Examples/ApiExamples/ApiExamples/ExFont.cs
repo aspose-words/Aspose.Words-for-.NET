451f3879a2498d38b58719214c5775b03707a49c
Alexander Dudin <alexander.dudin@aspose.com>
2021-03-18 21:24:22 +0300

    Removed non-generic Array and IEnumerator

--- Examples/ApiExamples/ApiExamples/ExFont.cs
+++ Examples/ApiExamples/ApiExamples/ExFont.cs
@@ -8,6 +8,7 @@
 #if NET462 || NETCOREAPP2_1 || JAVA
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Globalization;
 using System.IO;
@@ -1346,10 +1347,10 @@ namespace ApiExamples
             //ExSummary:Shows how to access and print details of each font in a document.
             Document doc = new Document(MyDir + "Document.docx");
             
-            IEnumerator fontCollectionEnumerator = doc.FontInfos.GetEnumerator();
+            IEnumerator<FontInfo> fontCollectionEnumerator = doc.FontInfos.GetEnumerator();
             while (fontCollectionEnumerator.MoveNext())
             {
-                FontInfo fontInfo = (FontInfo)fontCollectionEnumerator.Current;
+                FontInfo fontInfo = fontCollectionEnumerator.Current;
                 if (fontInfo != null)
                 {
                     Console.WriteLine("Font name: " + fontInfo.Name);
