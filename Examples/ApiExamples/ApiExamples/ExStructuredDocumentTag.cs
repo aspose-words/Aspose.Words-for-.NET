cfeaf38726a32b3be224c8d67a30e409910fb4a8
vderyushev <falleretic@gmail.com>
2021-07-02 15:59:32 +0700

    Added examples for API 21.5

--- Examples/ApiExamples/ApiExamples/ExStructuredDocumentTag.cs
+++ Examples/ApiExamples/ApiExamples/ExStructuredDocumentTag.cs
@@ -90,6 +90,8 @@ namespace ApiExamples
             //ExStart
             //ExFor:StructuredDocumentTag.#ctor(DocumentBase, SdtType, MarkupLevel)
             //ExFor:StructuredDocumentTag.Checked
+            //ExFor:StructuredDocumentTag.SetCheckedSymbol(System.Int32, System.String)
+            //ExFor:StructuredDocumentTag.SetUncheckedSymbol(System.Int32, System.String)
             //ExSummary:Show how to create a structured document tag in the form of a check box.
             Document doc = new Document();
             DocumentBuilder builder = new DocumentBuilder(doc);
@@ -97,6 +99,10 @@ namespace ApiExamples
             StructuredDocumentTag sdtCheckBox =
                 new StructuredDocumentTag(doc, SdtType.Checkbox, MarkupLevel.Inline) {Checked = true};
 
+            // We can set the symbols used to represent the checked/unchecked state of a checkbox content control.
+            sdtCheckBox.SetCheckedSymbol(0x00A9, "Times New Roman");
+            sdtCheckBox.SetUncheckedSymbol(0x00AE, "Times New Roman");
+            
             builder.InsertNode(sdtCheckBox);
 
             doc.Save(ArtifactsDir + "StructuredDocumentTag.CheckBox.docx");
