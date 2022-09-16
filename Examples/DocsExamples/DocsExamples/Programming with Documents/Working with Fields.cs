628605b07ee30ff913fb14c4017b54e75ad04199
vderyushev <vyacheslav.deryushev@aspose.com>
2022-09-16 15:45:55 +0300

    Updated example

--- Examples/DocsExamples/DocsExamples/Programming with Documents/Working with Fields.cs
+++ Examples/DocsExamples/DocsExamples/Programming with Documents/Working with Fields.cs
@@ -83,103 +83,19 @@ namespace DocsExamples.Programming_with_Documents
             builder.InsertField(@"MERGEFIELD MyMergeField1 \* MERGEFORMAT");
             builder.InsertField(@"MERGEFIELD MyMergeField2 \* MERGEFORMAT");
 
-            // Select all field start nodes so we can find the merge fields.
-            NodeCollection fieldStarts = doc.GetChildNodes(NodeType.FieldStart, true);
-            foreach (FieldStart fieldStart in fieldStarts)
+            foreach (Field f in doc.Range.Fields)
             {
-                if (fieldStart.FieldType == FieldType.FieldMergeField)
+                if (f.Type == FieldType.FieldMergeField)
                 {
-                    MergeField mergeField = new MergeField(fieldStart);
-                    mergeField.Name += "_Renamed";
+                    FieldMergeField mergeField = (FieldMergeField)f;
+                    mergeField.FieldName = mergeField.FieldName + "_Renamed";
+                    mergeField.Update();
                 }
             }
 
-            doc.Save(ArtifactsDir + "WorkingWithFields.RenameMergeFields.doc");
+            doc.Save(ArtifactsDir + "WorkingWithFields.RenameMergeFields.docx");
             //ExEnd:RenameMergeFields
         }
-
-        //ExStart:MergeField
-        /// <summary>
-        /// Represents a facade object for a merge field in a Microsoft Word document.
-        /// </summary>
-        internal class MergeField
-        {
-            internal MergeField(FieldStart fieldStart)
-            {
-                if (fieldStart == null)
-                    throw new ArgumentNullException(nameof(fieldStart));
-                if (fieldStart.FieldType != FieldType.FieldMergeField)
-                    throw new ArgumentException("Field start type must be FieldMergeField.");
-
-                mFieldStart = fieldStart;
-
-                // Find the field separator node.
-                mFieldSeparator = fieldStart.GetField().Separator;
-                if (mFieldSeparator == null)
-                    throw new InvalidOperationException("Cannot find field separator.");
-
-                mFieldEnd = fieldStart.GetField().End;
-            }
-
-            /// <summary>
-            /// Gets or sets the name of the merge field.
-            /// </summary>
-            internal string Name
-            {
-                get => ((FieldStart) mFieldStart).GetField().Result.Replace("«", "").Replace("»", "");
-                set
-                {
-                    // Merge field name is stored in the field result which is a Run
-                    // node between field separator and field end.
-                    Run fieldResult = (Run) mFieldSeparator.NextSibling;
-                    fieldResult.Text = string.Format("«{0}»", value);
-
-                    // But sometimes the field result can consist of more than one run, delete these runs.
-                    RemoveSameParent(fieldResult.NextSibling, mFieldEnd);
-
-                    UpdateFieldCode(value);
-                }
-            }
-
-            private void UpdateFieldCode(string fieldName)
-            {
-                // Field code is stored in a Run node between field start and field separator.
-                Run fieldCode = (Run) mFieldStart.NextSibling;
-
-                Match match = gRegex.Match(((FieldStart) mFieldStart).GetField().GetFieldCode());
-
-                string newFieldCode = string.Format(" {0}{1} ", match.Groups["start"].Value, fieldName);
-                fieldCode.Text = newFieldCode;
-
-                // But sometimes the field code can consist of more than one run, delete these runs.
-                RemoveSameParent(fieldCode.NextSibling, mFieldSeparator);
-            }
-
-            /// <summary>
-            /// Removes nodes from start up to but not including the end node.
-            /// Start and end are assumed to have the same parent.
-            /// </summary>
-            private void RemoveSameParent(Node startNode, Node endNode)
-            {
-                if (endNode != null && startNode.ParentNode != endNode.ParentNode)
-                    throw new ArgumentException("Start and end nodes are expected to have the same parent.");
-
-                Node curChild = startNode;
-                while (curChild != null && curChild != endNode)
-                {
-                    Node nextChild = curChild.NextSibling;
-                    curChild.Remove();
-                    curChild = nextChild;
-                }
-            }
-
-            private readonly Node mFieldStart;
-            private readonly Node mFieldSeparator;
-            private readonly Node mFieldEnd;
-
-            private readonly Regex gRegex = new Regex(@"\s*(?<start>MERGEFIELD\s|)(\s|)(?<name>\S+)\s+");
-        }
-        //ExEnd:MergeField
 
         [Test]
         public void RemoveField()
