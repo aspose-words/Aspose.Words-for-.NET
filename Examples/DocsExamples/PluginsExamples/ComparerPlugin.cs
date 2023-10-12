24c39472646a7688786ed65691d5213ba9c7d0cc
vderyushev <falleretic@gmail.com>
2023-10-12 14:18:02 +0300

    Updated CompareDocuments example

--- Examples/DocsExamples/PluginsExamples/ComparerPlugin.cs
+++ Examples/DocsExamples/PluginsExamples/ComparerPlugin.cs
@@ -11,13 +11,19 @@ namespace PluginsExamples
         {
             //ExStart:CompareDocuments
             //GistId:fe5b5017bb9ffbf8a2619a4d90baff33
-            var docA = new Document(MyDir + "Document.docx");
-            var docB = new Document(MyDir + "Blank.docx");
+            var docA = new Document(MyDir + "Blank.docx");
+            var docB = new Document(MyDir + "Document.docx");
 
             // docA now contains changes as revisions.
             docA.Compare(docB, "User", DateTime.Now, new CompareOptions { IgnoreFormatting = true });
 
-            Console.WriteLine(docA.Revisions.Count == 0 ? "Documents are equal" : "Documents are not equal");
+            foreach (Revision revision in docA.Revisions)
+            {
+                Console.WriteLine("Type: " + revision.RevisionType);
+                Console.WriteLine("Author: " + revision.Author);
+                Console.WriteLine("Date: " + revision.DateTime);
+                Console.WriteLine("Revision text: " + revision.ParentNode.ToString(SaveFormat.Text));                
+            }
             //ExEnd:CompareDocuments
         }
     }
