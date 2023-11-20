641196a03d47f24295f0600b0f09617217363788
vderyushev <falleretic@gmail.com>
2023-11-20 12:58:52 +0300

    Added Delphi project to showcases

---
+++ Showcases/Delphi_COM_by_Aspose_Words_for_NET/Aspose.Words.Examples.dpr
@@ -0,0 +1,43 @@
+program Aspose.Words.Examples;
+
+uses
+  System.SysUtils,
+  System.Win.ComObj,
+  Aspose_Words_TLB in '..\22.0\Imports\Aspose_Words_TLB.pas';
+
+procedure ConvertDocument();
+var
+  helper, doc: OLEvariant;
+begin
+  helper:= CreateOleObject('Aspose.Words.ComHelper');
+  doc := helper.Open('X:\Aspose.Words-for-.NET.git\Examples\Data\Absolute position tab.docx');
+  doc.Save('X:\Aspose.Words-for-.NET.git\Examples\Data\Absolute position tab.pdf');
+end;
+
+procedure CreateNewDocument();
+var
+  builder: OLEvariant;
+begin
+  builder := CreateOleObject('Aspose.Words.DocumentBuilder');
+  builder.Writeln('Hello world!');
+  builder.Document.Save('X:\Aspose.Words-for-.NET.git\Examples\Data\New document.docx');
+end;
+
+
+var
+  license : _License;
+begin
+  Set8087CW($133f);
+  try
+    license := CoLicense.Create;
+    license.SetLicense('X:\Aspose.Words-for-.NET.git\Examples\Data\License\Aspose.Words.NET.lic');
+
+    ConvertDocument();
+    CreateNewDocument();
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
+
+
---
+++ Showcases/Delphi_COM_by_Aspose_Words_for_NET/README.md
@@ -0,0 +1,13 @@
+# Delphi COM by Aspose.Words for .NET
+Shows how to use Aspose.Words for .NET via COM Interop in Delphi. 
+
+##How to create and add tlb file
+1. Create tlb file read more on our [documentation website](https://docs.aspose.com/words/net/how-to-use-aspose-words-via-com-interop/).
+2. In RAD studio click Component -> Import component -> Import a type library, choose Aspose.Words and add to current project.
+
+##Examples
+
+ - Convert document
+ - Create new document
+ 
+
