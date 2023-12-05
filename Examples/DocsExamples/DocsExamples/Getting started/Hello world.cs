cb7c700a6115a18b4b19966e11f560b676c2f5b6
vderyushev <falleretic@gmail.com>
2023-12-05 10:56:24 +0300

    Added HelloWorld example

---
+++ Examples/DocsExamples/DocsExamples/Getting started/Hello world.cs
@@ -0,0 +1,28 @@
+﻿using Aspose.Words;
+using NUnit.Framework;
+
+namespace DocsExamples.Getting_started
+{
+    public class Hello_world : DocsExamplesBase
+    {
+        [Test]
+        public void HelloWorld()
+        {
+            //ExStart:HelloWorld
+            //GistDesc:Getting started example
+            Document docA = new Document();            
+            DocumentBuilder builder = new DocumentBuilder(docA);
+
+            // Insert text to the document start.
+            builder.MoveToDocumentStart();
+            builder.Write("First Hello World paragraph");
+
+            Document docB = new Document(MyDir + "Document.docx");
+            // Add document B to the and of document A, preserving document B formatting.
+            docA.AppendDocument(docB, ImportFormatMode.KeepSourceFormatting);
+            
+            docA.Save(ArtifactsDir + "C:\\Temp\\output_AB.pdf");
+            //ExEnd:HelloWorld
+        }
+    }
+}
