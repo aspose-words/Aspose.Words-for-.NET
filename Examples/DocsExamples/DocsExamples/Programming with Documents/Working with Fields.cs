d448110da25e5e66f363fca1c5adaf82f150e1eb
vderyushev <vyacheslav.deryushev@aspose.com>
2025-01-29 17:56:45 +0300

    Updated "Apply Custom Formatting"

--- Examples/DocsExamples/DocsExamples/Programming with Documents/Working with Fields.cs
+++ Examples/DocsExamples/DocsExamples/Programming with Documents/Working with Fields.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Threading;
@@ -449,7 +450,8 @@ namespace DocsExamples.Programming_with_Documents
         [Test]
         public void FieldUpdateCulture()
         {
-            //ExStart:FieldUpdateCultureProvider
+            //ExStart:FieldUpdateCulture
+            //GistId:79b46682fbfd7f02f64783b163ed95fc
             Document doc = new Document();
             DocumentBuilder builder = new DocumentBuilder(doc);
 
@@ -459,10 +461,11 @@ namespace DocsExamples.Programming_with_Documents
             doc.FieldOptions.FieldUpdateCultureProvider = new FieldUpdateCultureProvider();
 
             doc.Save(ArtifactsDir + "WorkingWithFields.FieldUpdateCulture.pdf");
-            //ExEnd:FieldUpdateCultureProvider
+            //ExEnd:FieldUpdateCulture
         }
 
-        //ExStart:FieldUpdateCultureProviderGetCulture
+        //ExStart:FieldUpdateCultureProvider
+        //GistId:79b46682fbfd7f02f64783b163ed95fc
         class FieldUpdateCultureProvider : IFieldUpdateCultureProvider
         {
             public CultureInfo GetCulture(string name, Field field)
@@ -512,7 +515,7 @@ namespace DocsExamples.Programming_with_Documents
                 }
             }
         }
-        //ExEnd:FieldUpdateCultureProviderGetCulture
+        //ExEnd:FieldUpdateCultureProvider
 
         [Test]
         public void FieldDisplayResults()
@@ -532,16 +535,17 @@ namespace DocsExamples.Programming_with_Documents
         }
 
         [Test]
-        public void EvaluateIFCondition()
+        public void EvaluateIfCondition()
         {
-            //ExStart:EvaluateIFCondition
+            //ExStart:EvaluateIfCondition
+            //GistId:79b46682fbfd7f02f64783b163ed95fc
             DocumentBuilder builder = new DocumentBuilder();
 
             FieldIf field = (FieldIf) builder.InsertField("IF 1 = 1", null);
             FieldIfComparisonResult actualResult = field.EvaluateCondition();
 
             Console.WriteLine(actualResult);
-            //ExEnd:EvaluateIFCondition
+            //ExEnd:EvaluateIfCondition
         }
 
         [Test]
@@ -624,5 +628,139 @@ namespace DocsExamples.Programming_with_Documents
             compositeNode.Range.Fields.Cast<Field>().Where(f => f.Type == targetFieldType).ToList().ForEach(f => f.Unlink());
         }
         //ExEnd:ConvertFieldsToStaticText
+
+        [Test]
+        public void FieldResultFormatting()
+        {
+            //ExStart:FieldResultFormatting
+            //GistId:79b46682fbfd7f02f64783b163ed95fc
+            Document doc = new Document();
+            DocumentBuilder builder = new DocumentBuilder(doc);
+            FieldResultFormatter formatter = new FieldResultFormatter("${0}", "Date: {0}", "Item # {0}:");
+            doc.FieldOptions.ResultFormatter = formatter;
+
+            // Our field result formatter applies a custom format to newly created fields of three types of formats.
+            // Field result formatters apply new formatting to fields as they are updated,
+            // which happens as soon as we create them using this InsertField method overload.
+            // 1 -  Numeric:
+            builder.InsertField(" = 2 + 3 \\# $###");
+
+            Assert.AreEqual("$5", doc.Range.Fields[0].Result);
+            Assert.AreEqual(1, formatter.CountFormatInvocations(FieldResultFormatter.FormatInvocationType.Numeric));
+
+            // 2 -  Date/time:
+            builder.InsertField("DATE \\@ \"d MMMM yyyy\"");
+
+            Assert.IsTrue(doc.Range.Fields[1].Result.StartsWith("Date: "));
+            Assert.AreEqual(1, formatter.CountFormatInvocations(FieldResultFormatter.FormatInvocationType.DateTime));
+
+            // 3 -  General:
+            builder.InsertField("QUOTE \"2\" \\* Ordinal");
+
+            Assert.AreEqual("Item # 2:", doc.Range.Fields[2].Result);
+            Assert.AreEqual(1, formatter.CountFormatInvocations(FieldResultFormatter.FormatInvocationType.General));
+
+            formatter.PrintFormatInvocations();
+            //ExEnd:FieldResultFormatting
+        }
+
+        //ExStart:FieldResultFormatter
+        //GistId:79b46682fbfd7f02f64783b163ed95fc
+        /// <summary>
+        /// When fields with formatting are updated, this formatter will override their formatting
+        /// with a custom format, while tracking every invocation.
+        /// </summary>
+        private class FieldResultFormatter : IFieldResultFormatter
+        {
+            public FieldResultFormatter(string numberFormat, string dateFormat, string generalFormat)
+            {
+                mNumberFormat = numberFormat;
+                mDateFormat = dateFormat;
+                mGeneralFormat = generalFormat;
+            }
+
+            public string FormatNumeric(double value, string format)
+            {
+                if (string.IsNullOrEmpty(mNumberFormat))
+                    return null;
+
+                string newValue = String.Format(mNumberFormat, value);
+                FormatInvocations.Add(new FormatInvocation(FormatInvocationType.Numeric, value, format, newValue));
+                return newValue;
+            }
+
+            public string FormatDateTime(DateTime value, string format, CalendarType calendarType)
+            {
+                if (string.IsNullOrEmpty(mDateFormat))
+                    return null;
+
+                string newValue = String.Format(mDateFormat, value);
+                FormatInvocations.Add(new FormatInvocation(FormatInvocationType.DateTime, $"{value} ({calendarType})", format, newValue));
+                return newValue;
+            }
+
+            public string Format(string value, GeneralFormat format)
+            {
+                return Format((object)value, format);
+            }
+
+            public string Format(double value, GeneralFormat format)
+            {
+                return Format((object)value, format);
+            }
+
+            private string Format(object value, GeneralFormat format)
+            {
+                if (string.IsNullOrEmpty(mGeneralFormat))
+                    return null;
+
+                string newValue = String.Format(mGeneralFormat, value);
+                FormatInvocations.Add(new FormatInvocation(FormatInvocationType.General, value, format.ToString(), newValue));
+                return newValue;
+            }
+
+            public int CountFormatInvocations(FormatInvocationType formatInvocationType)
+            {
+                if (formatInvocationType == FormatInvocationType.All)
+                    return FormatInvocations.Count;
+                return FormatInvocations.Count(f => f.FormatInvocationType == formatInvocationType);
+            }
+
+            public void PrintFormatInvocations()
+            {
+                foreach (FormatInvocation f in FormatInvocations)
+                    Console.WriteLine($"Invocation type:\t{f.FormatInvocationType}\n" +
+                                      $"\tOriginal value:\t\t{f.Value}\n" +
+                                      $"\tOriginal format:\t{f.OriginalFormat}\n" +
+                                      $"\tNew value:\t\t\t{f.NewValue}\n");
+            }
+
+            private readonly string mNumberFormat;
+            private readonly string mDateFormat;
+            private readonly string mGeneralFormat;
+            private List<FormatInvocation> FormatInvocations { get; } = new List<FormatInvocation>();
+
+            private class FormatInvocation
+            {
+                public FormatInvocationType FormatInvocationType { get; }
+                public object Value { get; }
+                public string OriginalFormat { get; }
+                public string NewValue { get; }
+
+                public FormatInvocation(FormatInvocationType formatInvocationType, object value, string originalFormat, string newValue)
+                {
+                    Value = value;
+                    FormatInvocationType = formatInvocationType;
+                    OriginalFormat = originalFormat;
+                    NewValue = newValue;
+                }
+            }
+
+            public enum FormatInvocationType
+            {
+                Numeric, DateTime, General, All
+            }
+        }
+        //ExEnd:FieldResultFormatter
     }
 }
